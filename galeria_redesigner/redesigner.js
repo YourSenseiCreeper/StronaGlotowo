b899f347ff67039988044059f76acb0be8ac1d40
Piotr Wereszczyński <6324814+YourSenseiCreeper@users.noreply.github.com>
2024-03-12 15:28:31 +0100

    Galeria redesigner prototype

--- galeria_redesigner/redesigner.js
+++ galeria_redesigner/redesigner.js
@@ -1,9 +1,4 @@
-var albumPath = '../galeria/album2'
-var title = "Forum 2004";
-var slidesFolder = `${albumPath}/slides`;
 var htmlTemplateFile = './index_template.html';
-var targetHtmlFile = `${albumPath}/new_index.html`;
-
 const fs = require('fs');
 
 function createGalleryFromFiles(files) {
@@ -11,23 +6,40 @@ function createGalleryFromFiles(files) {
     return files.map(f => `<img src="./slides/${f}" alt="${f}" onclick="openImage('${f}')">`);
 }
 
-fs.readdir(slidesFolder, (err, files) => {
-  let jpgFiles = files.filter(f => f.toLowerCase().endsWith('.jpg'))
-  let results = createGalleryFromFiles(jpgFiles).join('\n');
-//   results.forEach(file => {
-//     console.log(file);
-//   });
+function redesign(albumPath, title) {
+  let slidesFolder = `${albumPath}/slides`;
+  let targetHtmlFile = `${albumPath}/new_index.html`;
 
-  fs.readFile(htmlTemplateFile, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-
-    var result = data.replace(/{title}/g, title).replace(/{content}/g, results);
+  fs.readdir(slidesFolder, (err, files) => {
+    let jpgFiles = files.filter(f => f.toLowerCase().endsWith('.jpg'))
+    let results = createGalleryFromFiles(jpgFiles).join('\n');
   
-    fs.writeFile(targetHtmlFile, result, 'utf8', function (err) {
-       if (err) return console.log(err);
+    fs.readFile(htmlTemplateFile, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+  
+      let imagesMap = jpgFiles.map(f => `'${f}'`).join(',');
+      let imageInitInScript = `images = [${imagesMap}];`;
+      var result = data.replace(/{title}/g, title).replace(/{content}/g, results).replace(/{initImageGallery}/g, imageInitInScript);
+    
+      fs.writeFile(targetHtmlFile, result, 'utf8', function (err) {
+         if (err) return console.log(err);
+      });
     });
   });
-});
+  
+  fs.copyFile('style.css', albumPath + '/style.css', (err) => { if (err) return console.log(err); });
+  fs.copyFile('script.js', albumPath + '/script.js', (err) => { if (err) return console.log(err); });
+}
 
+// redesign('../galeria/album2', 'Forum 2004');
+// redesign('../galeria/album3', 'Głotowo z okna');
+// redesign('../galeria/album4', 'Kościół Głotowo 2004');
+// redesign('../galeria/album5', 'Przedwojenne widokówki z Głotowa');
+// redesign('../galeria/album6', 'Wiosna 2004');
+// redesign('../galeria/album7', 'Forum Ewangelizacyjne 2003');
+// redesign('../galeria/album9', 'Spotkanie 20 lat po maturze - 13.05.2006 r.');
+// redesign('../galeria/album10', 'Swobodno - nabożeństwo majowe');
+// redesign('../galeria/album16', 'Zdjęcia Dom Pielgrzyma');
+redesign('../galeria/album17', 'Forum Ewangelizacyjne 2008');
--- galeria_redesigner/script.js
+++ galeria_redesigner/script.js
@@ -1,7 +1,12 @@
+var imageIndex = 0;
+var images = [];
+
 function openImage(filename) {
     let img = document.getElementById('fullscreen-image');
     img.src = './slides/' + filename;
     
+    imageIndex = images.indexOf(filename);
+
     let wrapper = document.getElementById('fullscreen-image-wrapper');
     wrapper.classList.remove('wrapper-hidden');
 }
@@ -9,4 +14,24 @@ function openImage(filename) {
 function closeWrapper() {
     let wrapper = document.getElementById('fullscreen-image-wrapper');
     wrapper.classList.add('wrapper-hidden');
+}
+
+function nextImage() {
+    if (imageIndex >= images.length){
+        imageIndex = 0;
+    } else {
+        imageIndex++;
+    }
+    let img = document.getElementById('fullscreen-image');
+    img.src = './slides/' + images[imageIndex];
+}
+
+function previousImage() {
+    if (imageIndex <= 0){
+        imageIndex = images.length - 1; // sprawdzić czy ok
+    } else {
+        imageIndex--;
+    }
+    let img = document.getElementById('fullscreen-image');
+    img.src = './slides/' + images[imageIndex];
 }
