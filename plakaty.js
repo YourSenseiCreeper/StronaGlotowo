8ac0d5484a265c2960161a36fe8dd34eaf52124d
Piotr Wereszczyński <6324814+YourSenseiCreeper@users.noreply.github.com>
2024-10-23 16:29:01 +0200

    Add static files

--- plakaty.js
+++ plakaty.js
@@ -1,5 +1,4 @@
 var szerokosc = "200px"; 
 var plakaty = [
     new Plakat('Głotowo 2024', './obrazki/plakaty/Glotowo 2024.jpg'),
-    new Plakat('Wielki Post 2024 w Głotowie', './obrazki/plakaty/Wielki post 2024.jpg'),
 ];
