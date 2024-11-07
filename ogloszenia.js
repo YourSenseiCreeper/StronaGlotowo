a2fd60a44a17f146b008c0d914b54d574d1bddea
mproszek <mproszek@op.pl>
2024-11-07 19:51:34 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,4 +1,4 @@
 var ogloszeniaNaglowek = '31 NIEDZIELA ZWYKŁA B - 3.11.2024 R.';
 var ogloszenia = [
-    'Bóg zapłać za ofiary na fundusz remontowy.'
+    'Bóg zapłać za ofiary na fundusz remontowy'
 ];
