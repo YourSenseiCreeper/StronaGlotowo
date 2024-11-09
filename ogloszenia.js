451d5d5a6a176e4343a3b433e8bc126e97368e5f
mproszek <mproszek@op.pl>
2024-11-09 19:47:05 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,4 +1,6 @@
-var ogloszeniaNaglowek = '31 NIEDZIELA ZWYKŁA B - 3.11.2024 R.';
+var ogloszeniaNaglowek = '32 NIEDZIELA ZWYKŁA B - 10.11.2024 R.';
 var ogloszenia = [
     'Bóg zapłać za ofiary na fundusz remontowy'
+1. Jutro Msza w Dzień Niepodległości o 11.00.
+2. Wypominki
 ];
