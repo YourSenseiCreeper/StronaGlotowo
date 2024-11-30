64de18f9e575f29303ecbaf7145b5b97af69e2bf
mproszek <mproszek@op.pl>
2024-12-01 00:36:00 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,6 +1,6 @@
-var ogloszeniaNaglowek = '34 NIEDZIELA CHRYSTUSA KRÓŁA B - 24.11.2024 R.';
+var ogloszeniaNaglowek = 'I NIEDZIELA ADWENTU B - 1.12.2024 R.';
 var ogloszenia = [
-    'Bóg zapłać za ofiary na fundusz remontowy',
-'Jutro Msza w kaplicy u Sióstr o 11.00.',
-'Dziś po Sumie o 11 nabożeństwu zawierzenia Chrystusowi Królowi',
+    'Dziś po Mszy o 8.30 Adoracja Eucharystyczna i zmiana tajemnic różańca',
+'W tym tygodniu zapraszamy na Roraty w środę i w piątek o 18.Dzieci zapraszamy szczególnie w piatek.W tym tygodniu wypada I piątek i I sobota miesiąca. Nabożeństwo po Mszy.  ',
+'W zakrystii do nabycia są świce wigilijne jako pomoc dla ubogich po 10 zł oraz poświęcone opłatki za dobrowolną ofiarę na ogrzewanie i prąd w kościele.',
 ];
