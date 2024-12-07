2250da171113d17572f00c01b88cfb7e5a9ac5c5
mproszek <mproszek@op.pl>
2024-12-07 23:37:57 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,6 +1,6 @@
-var ogloszeniaNaglowek = 'I NIEDZIELA ADWENTU B - 1.12.2024 R.';
+var ogloszeniaNaglowek = 'II NIEDZIELA ADWENTU - NIEPOKALANE POCZĘCIE NMP B - 8.12.2024 R.';
 var ogloszenia = [
-    'Dziś po Mszy o 8.30 Adoracja Eucharystyczna i zmiana tajemnic różańca',
-'W tym tygodniu zapraszamy na Roraty w środę i w piątek o 18.Dzieci zapraszamy szczególnie w piatek.W tym tygodniu wypada I piątek i I sobota miesiąca. Nabożeństwo po Mszy.  ',
-'W zakrystii do nabycia są świce wigilijne jako pomoc dla ubogich po 10 zł oraz poświęcone opłatki za dobrowolną ofiarę na ogrzewanie i prąd w kościele.',
+    'Dziś po Mszy o 11.00 spotkanie rodziców i dzieci przed I Komunią. W przyszłą niedzielę spotkanie rodziców i młodzieży przed Bierzmowaniem po Mszy o 11.00 ',
+'W tym tygodniu zapraszamy dzieci, młodzież i dorosłych na Roraty w środę i w piątek o 18.',
+'W zakrystii do nabycia są świce wigilijne jako pomoc dla ubogich po 10 zł oraz poświęcone opłatki za dobrowolną ofiarę na ogrzewanie i prąd w kościele. W przyszłą niedzielę ofiary na fundusz remontowy',
 ];
