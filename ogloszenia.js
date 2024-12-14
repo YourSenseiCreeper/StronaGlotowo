6617ef8d360d8264b692942271905587fb83fcd9
mproszek <mproszek@op.pl>
2024-12-14 21:49:31 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,6 +1,7 @@
-var ogloszeniaNaglowek = 'II NIEDZIELA ADWENTU - NIEPOKALANE POCZĘCIE NMP B - 8.12.2024 R.';
+var ogloszeniaNaglowek = 'III NIEDZIELA ADWENTU - C - 15.12.2024 R.';
 var ogloszenia = [
-    'Dziś po Mszy o 11.00 spotkanie rodziców i dzieci przed I Komunią. W przyszłą niedzielę spotkanie rodziców i młodzieży przed Bierzmowaniem po Mszy o 11.00 ',
+    'Dziś po Mszy o 11.00 spotkanie rodziców i młodzieży przed Bierzmowaniem. ',
 'W tym tygodniu zapraszamy dzieci, młodzież i dorosłych na Roraty w środę i w piątek o 18.',
-'W zakrystii do nabycia są świce wigilijne jako pomoc dla ubogich po 10 zł oraz poświęcone opłatki za dobrowolną ofiarę na ogrzewanie i prąd w kościele. W przyszłą niedzielę ofiary na fundusz remontowy',
+'W najbliższa sobotę 21.12. przeżywać będziemy nasz Adwentowy Dzień Rekolekcyjny, który poprowadzi ks. kan Jan Pietrzyk. Msze z okazja do spowiedzi o 11.00 i 18.00. Po 13.00 Odwiedziny chorych - proszę zgłaszać. O 15 zapraszam do przygotowania żłóbka i choinek.Ofiary w tym dniu przekazujemy na cele duszpasterskie ks. Jana. Tradycynie proszę o dary na stół rekolekcyjny.',
+'W zakrystii do nabycia są świce wigilijne jako pomoc dla ubogich po 10 zł oraz poświęcone opłatki za dobrowolną ofiarę na ogrzewanie i prąd w kościele. Bóg zapłać za ofiary na fundusz remontowy - odbiór prac',
 ];
