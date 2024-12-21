b8fc1987a0a554052fe5917058947e5d3df21e4e
mproszek <mproszek@op.pl>
2024-12-21 23:21:23 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,7 +1,7 @@
-var ogloszeniaNaglowek = 'III NIEDZIELA ADWENTU - C - 15.12.2024 R.';
+var ogloszeniaNaglowek = 'IV NIEDZIELA ADWENTU - C - 22.12.2024 R.';
 var ogloszenia = [
-    'Dziś po Mszy o 11.00 spotkanie rodziców i młodzieży przed Bierzmowaniem. ',
-'W tym tygodniu zapraszamy dzieci, młodzież i dorosłych na Roraty w środę i w piątek o 18.',
-'W najbliższa sobotę 21.12. przeżywać będziemy nasz Adwentowy Dzień Rekolekcyjny, który poprowadzi ks. kan Jan Pietrzyk. Msze z okazja do spowiedzi o 11.00 i 18.00. Po 13.00 Odwiedziny chorych - proszę zgłaszać. O 15 zapraszam do przygotowania żłóbka i choinek.Ofiary w tym dniu przekazujemy na cele duszpasterskie ks. Jana. Tradycynie proszę o dary na stół rekolekcyjny.',
-'W zakrystii do nabycia są świce wigilijne jako pomoc dla ubogich po 10 zł oraz poświęcone opłatki za dobrowolną ofiarę na ogrzewanie i prąd w kościele. Bóg zapłać za ofiary na fundusz remontowy - odbiór prac',
+    'We wtorek wigilia. Nie zapomnijmy o czytaniu ewangelii o Narodzeniu Pańskim, modlitwie, kolędach i dzieleniu się opłatkiem. Pasterka o 24.00 ',
+'W I dzień świąt Suma o 11.00, w II dzień świąt Msze o 8.30 i 11. Ofiary na KUL.W następną Niedzielę Św. Rodziny odnowienie przyżeczeń małżeńskich',
+'Dziękuję OSP, dorosłym i młodzieży w przygotowaniu żłóbka i choinek.',
+'W zakrystii do nabycia są świce wigilijne jako pomoc dla ubogich po 10 zł oraz poświęcone opłatki za dobrowolną ofiarę na ogrzewanie i prąd w kościele.',
 ];
