137b889c5948dae234ecc9ddeabdada016db402a
mproszek <mproszek@op.pl>
2025-01-04 12:34:09 +0100

    Test

--- aktualnosci.js
+++ aktualnosci.js
@@ -1,42 +1,37 @@
-var aktualnosciNaglowek = 'AKTUALNOŚCI 2024 R.';
+var aktualnosciNaglowek = 'AKTUALNOŚCI 2025 R.';
 var aktualnosci = [
-    new Aktualnosc('PIĄTKI WIELKIEGO POSTU 2024', 'GODZINY MIŁOSIERDZIA BOŻEGO',
+    new Aktualnosc('PIĄTKI WIELKIEGO POSTU 2025', 'GODZINY MIŁOSIERDZIA BOŻEGO',
     [
         '17.00 - DROGA KRZYŻOWA NA KALWARII WARMIŃSKIEJ,',
         '18.00 - MSZA ŚW Z MODLITWĄ O UZDROWIENIE DUSZY I CIAŁA.'
     ]),
-    new Aktualnosc('21-23.03.2024', 'REKOLEKCJE WIELKOPOSTNE',
+    new Aktualnosc('10-12.04.2025', 'REKOLEKCJE WIELKOPOSTNE',
     [
         '- 11.00 - Msza Św. z nauką rekolekcyjną.',
         '- 18.00 - Msza Św. z nauką rekolekcyjną.'
     ]),
-    new Aktualnosc('28-31.03.2024', 'Triduum Paschalne w Głotowie',
-    [
-        'To rekolekcje dla dorosłych pogłębiające przeżycie Świętych Dni przez dodatkowe katechezy, spotkania w grupach, nabożeństwa i modlitwy.',
-        'Początek w Wielki Czwartek o godz. 16.00.',
-        'Zakończenie w Niedzielę Wielkanocną po Rezurekcjach.'
-    ]),
-    new Aktualnosc('18.05.2024', '130 ROCZNICA POŚWIĘCENIA KALWARII WARMIŃSKIEJ I PIELGRZYMKA SOLIDARNOŚCI W HOŁDZIE śW. JANOWI PAWŁOWI II.',
+  
+    new Aktualnosc('17.05.2025', '131 ROCZNICA POŚWIĘCENIA KALWARII WARMIŃSKIEJ I PIELGRZYMKA SOLIDARNOŚCI W HOŁDZIE śW. JANOWI PAWŁOWI II.',
     [
         '- 11.00 - Uroczysta Msza Święta',
         '- 12.00 - Droga Krzyżowa'
     ]),
-    new Aktualnosc('2.06.2024', 'Odpust diecezjalny w Głotowie.',
+    new Aktualnosc('22.06.2025', 'Odpust diecezjalny w Głotowie.',
     [
 		'- 8.30 - Msza Św. z kazaniem',
         '- 10.00 - Droga Krzyżowa',
         '- 11.00 - Adoracja Najświętszego Sakramentu',
         '- 12.00 - Suma odpustowa'
     ]),
-    new Aktualnosc('15.09.2024', 'Odpust diecezjalny w Głotowie',
+    new Aktualnosc('21.09.2025', 'Odpust diecezjalny w Głotowie',
     [
         '- 8.30 - Msza Św. z kazaniem',
         '- 10.00 - Droga Krzyżowa',
         '- 11.00 - Adoracja Najświętszego Sakramentu',
         '- 12.00 - Suma odpustowa'
     ]),
-    new Aktualnosc('22.09.2024', 'Dożynki parafialne - Msza Św. o 11.00.', []),
-    new Aktualnosc('21.12.2024', 'ADWENTOWY DZIEŃ REKOLEKCYJNY.',
+    new Aktualnosc('28.09.2025', 'Dożynki parafialne - Msza Św. o 11.00.', []),
+    new Aktualnosc('20.12.2025', 'ADWENTOWY DZIEŃ REKOLEKCYJNY.',
     [
         '- Okazja do spowiedzi w czasie Mszy Świętych.',
         '- 11.00 - Msza Św. i nauka rekolekcyjna.',
--- ogloszenia.js
+++ ogloszenia.js
@@ -1,7 +1,6 @@
-var ogloszeniaNaglowek = 'NIEDZIELA ŚW. Rodziny - C - 29.12.2024 R.';
+var ogloszeniaNaglowek = '2 NIEDZIELA PO NARODZENIU PAŃSKIM - C - 5.01.2025 R.';
 var ogloszenia = [
-    'Jutro Msza o 10.00, We wtorek Msza z nabożeństwemo na zakończenie roku o 18.00 ',
-'1.01.25 W Uroczystość Św. Bożej Rodzicielki - święto makazane Msze  o 11.00 i 18.00.',
-'W tym tygodniu wypada I piątek i I sobota - Msze z nabożeństwami o 18.00.',
-'W następną niedzielę Msza o 8.30 za członków Róż Różańcowych i za parafian. Po niej Adoracja Eucharystyczna i zmiana tajemnic. PO Sumie spotkanie opłatkowe dla rad parafialnych i członków Róż Różańcowych na plebanii.',
+    'Jutro Uroczystość Objawienia Pańskiego - Trzech Króli Msze z poświęceniem kredy o 8.30 i 11.00, We wtorek Msza o 11.00 ',
+'We wtorek rozpoczynamy Wizytę duszpasterską tzw. kolędę. We wtorek ul. Szkolna od p. Matłosz do p. Dawidowicz od g. 15.00, w srodę ul. Szkolna Oś. za szkołą i od p. Bednarskiego do p. Sobotka od 15.00, w czwartek ul. Szkolna od p. Wesołowskich do p. Zboch od g.15.00, w piątek ul. Kościelna od p. Kotłowskiej do p. Klepaczko od g. 15.00, w sobotę ul. Kościelna od p. Kirkis do p. Wołodźko od g. 10.00 .',
+'Dziś w I niedzielę Msza o 8.30 za członków Róż Różańcowych i za parafian. Po niej Adoracja Eucharystyczna i zmiana tajemnic. Po Sumie spotkanie opłatkowe dla rad parafialnych i członków Róż Różańcowych na plebanii.',
 ];
