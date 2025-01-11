f4a13ecc62db7bf0575c62ff675c189037022031
mproszek <mproszek@op.pl>
2025-01-11 20:07:13 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,6 +1,5 @@
-var ogloszeniaNaglowek = '2 NIEDZIELA PO NARODZENIU PAŃSKIM - C - 5.01.2025 R.';
+var ogloszeniaNaglowek = 'NIEDZIELA CHRZTU PAŃSKIEGO - C - 12.01.2025 R.';
 var ogloszenia = [
-    'Jutro Uroczystość Objawienia Pańskiego - Trzech Króli Msze z poświęceniem kredy o 8.30 i 11.00, We wtorek Msza o 11.00 ',
-'We wtorek rozpoczynamy Wizytę duszpasterską tzw. kolędę. We wtorek ul. Szkolna od p. Matłosz do p. Dawidowicz od g. 15.00, w srodę ul. Szkolna Oś. za szkołą i od p. Bednarskiego do p. Sobotka od 15.00, w czwartek ul. Szkolna od p. Wesołowskich do p. Zboch od g.15.00, w piątek ul. Kościelna od p. Kotłowskiej do p. Klepaczko od g. 15.00, w sobotę ul. Kościelna od p. Kirkis do p. Wołodźko od g. 10.00 .',
-'Dziś w I niedzielę Msza o 8.30 za członków Róż Różańcowych i za parafian. Po niej Adoracja Eucharystyczna i zmiana tajemnic. Po Sumie spotkanie opłatkowe dla rad parafialnych i członków Róż Różańcowych na plebanii.',
+    'Kontynuujemy Wizytę duszpasterską tzw. kolędę. w poniedziałek kol. pn od p. Zajączkowskich do p. Pusz od 10..00. Trans. p. Tadeusz Symonowicz, We wtorek kol. pn od p. Napieralskich do p. Milkiewicz od10.00, trans. p. Piotr Napieralski, w srodę kol.pn od p. Stanulewicz do p. Bibik od 10.00, trans. p. Jerzy Napieralski,  w czwartek kol. pd od. p. Dąbrowskich do p. Stefańskich od 10.00, trans. p. Andrzej Stefański, w piątek Swobodna od 10.00.',
+'Dziś w I niedzielę Msza o 8.30 za członków Róż Różańcowych i za parafian. Po niej Adoracja Eucharystyczna i zmiana tajemnic. Za tydzień sprawozdanie za rok 2024. Taca na fundusz remontowy',
 ];
