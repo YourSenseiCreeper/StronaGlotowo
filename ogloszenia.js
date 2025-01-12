e9c18084c72356032087a586887aa9869943e30c
mproszek <mproszek@op.pl>
2025-01-13 00:49:27 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,5 +1,5 @@
 var ogloszeniaNaglowek = 'NIEDZIELA CHRZTU PAŃSKIEGO - C - 12.01.2025 R.';
 var ogloszenia = [
-    'Kontynuujemy Wizytę duszpasterską tzw. kolędę. w poniedziałek kol. pn od p. Zajączkowskich do p. Pusz od 10..00. Trans. p. Tadeusz Symonowicz, We wtorek kol. pn od p. Napieralskich do p. Milkiewicz od10.00, trans. p. Piotr Napieralski, w srodę kol.pn od p. Stanulewicz do p. Bibik od 10.00, trans. p. Jerzy Napieralski,  w czwartek kol. pd od. p. Dąbrowskich do p. Stefańskich od 10.00, trans. p. Andrzej Stefański, w piątek Swobodna od 10.00.',
-'Dziś w I niedzielę Msza o 8.30 za członków Róż Różańcowych i za parafian. Po niej Adoracja Eucharystyczna i zmiana tajemnic. Za tydzień sprawozdanie za rok 2024. Taca na fundusz remontowy',
+    'Kontynuujemy Wizytę duszpasterską tzw. kolędę. w poniedziałek kol. pn od p. Zajączkowskich do p. Pusz od 10..00. Trans. p. Tadeusz Symonowicz, We wtorek kol. pn od p. Napieralskich do p. Milkiewicz od10.00, trans. p. Piotr Napieralski, w srodę kol.pn od p. Stanulewicz do p. Bibik od 10.00, trans. p. Jerzy Napieralski,  w czwartek kol. pd od. p. Dąbrowskich do p. Stefańskich od 10.00, trans. p. Andrzej Stefański, w  Sobotę Swobodna od 10.00.',
+'Za tydzień sprawozdanie za rok 2024. Taca na fundusz remontowy',
 ];
