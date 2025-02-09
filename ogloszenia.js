c1c3ab0c440e083966c53f28bd0e75d86e18f1b8
mproszek <mproszek@op.pl>
2025-02-09 09:41:28 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,5 +1,5 @@
-var ogloszeniaNaglowek = 'NIEDZIELA CHRZTU PAŃSKIEGO - C - 12.01.2025 R.';
+var ogloszeniaNaglowek = '5 NIEDZIELA ZWYKŁA C - 9.02.2025 R.';
 var ogloszenia = [
-    'Kontynuujemy Wizytę duszpasterską tzw. kolędę. w poniedziałek kol. pn od p. Zajączkowskich do p. Pusz od 10..00. Trans. p. Tadeusz Symonowicz, We wtorek kol. pn od p. Napieralskich do p. Milkiewicz od10.00, trans. p. Piotr Napieralski, w srodę kol.pn od p. Stanulewicz do p. Bibik od 10.00, trans. p. Jerzy Napieralski,  w czwartek kol. pd od. p. Dąbrowskich do p. Stefańskich od 10.00, trans. p. Andrzej Stefański, w  Sobotę Swobodna od 10.00.',
-'Za tydzień sprawozdanie za rok 2024. Taca na fundusz remontowy',
+    'Bóg zapłać za sprzątanie żłóbka i choinek.',
+'Za tydzień ofiary na fundusz remontowy. Sprzątanie',
 ];
