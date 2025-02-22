f0cda139d280cd5976ab6bb3869517dc7908d45b
mproszek <mproszek@op.pl>
2025-02-22 18:16:30 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,5 +1,5 @@
-var ogloszeniaNaglowek = '5 NIEDZIELA ZWYKŁA C - 9.02.2025 R.';
+var ogloszeniaNaglowek = '7 NIEDZIELA ZWYKŁA C - 23.02.2025 R.';
 var ogloszenia = [
-    'Bóg zapłać za sprzątanie żłóbka i choinek.',
-'Za tydzień ofiary na fundusz remontowy. Sprzątanie',
+    'W najbliższą sobotę wypada I sobota miesiąca. Msza z nabożeństwem o 18.00.',
+'W następną niedzielę Msza o 8.30 w intencji Żywego Różańca i parafian, a po niej Adoracja Eucharystyczna i zmiana tajemnic. O 15.00 spotkanie rad parafialnych na plebanii.',
 ];
