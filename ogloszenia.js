aa11aee1fa4f9c1e99f3b66448b822ce7b31a47a
Piotr Wereszczyński <6324814+YourSenseiCreeper@users.noreply.github.com>
2025-02-22 20:15:35 +0100

    Poprawa daty

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,4 +1,4 @@
-var ogloszeniaNaglowek = '5 NIEDZIELA ZWYKŁA C - 9.02.2025 R.';
+var ogloszeniaNaglowek = '7 NIEDZIELA ZWYKŁA C - 23.02.2025 R.';
 var ogloszenia = [
     'W tym tygodniu wypada 1 sobota miesiąca. Msza z nabożeństwem o 18:00.',
 'W przyszłą niedzielę Msza w intencji Żywego Różańca i Parafian o 8:30. Po niej Adoracja Eucharystyczna i zmiana tajemnic.',
