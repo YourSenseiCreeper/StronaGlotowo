2b86a5e4cfa9c3b8f7b9a9cdd4403c558d37b91c
Piotr Wereszczyński <6324814+YourSenseiCreeper@users.noreply.github.com>
2025-02-22 19:56:36 +0100

    Aktualizacja ogłoszeń

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,5 +1,6 @@
 var ogloszeniaNaglowek = '5 NIEDZIELA ZWYKŁA C - 9.02.2025 R.';
 var ogloszenia = [
-    'Bóg zapłać za sprzątanie żłóbka i choinek.',
-'Za tydzień ofiary na fundusz remontowy. Sprzątanie',
+    'W tym tygodniu wypada 1 sobota miesiąca. Msza z nabożeństwem o 18:00.',
+'W przyszłą niedzielę Msza w intencji Żywego Różańca i Parafian o 8:30. Po niej Adoracja Eucharystyczna i zmiana tajemnic.',
+'W przyszłą niedzielę o 15:00 zapraszam członków naszych rad parafialnych na spotkanie na plebanię.'
 ];
