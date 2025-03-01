a3357f6077ad2261df7d7a0dc7080010535bcb24
mproszek <mproszek@op.pl>
2025-03-01 19:33:29 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -2,6 +2,6 @@ var ogloszeniaNaglowek = '8 NIEDZIELA ZWYKŁA C - 2.03.2025 R.';
 var ogloszenia = [
     'W najbliższą środę - Środa Popielcowa. Początek Wielkiego Postu. Msze z posypaniem głów popiołem o 11.00 i 18.00. Jest to dzień postu ścisłęgo. Osoby dorosłe od 18 r. do 60 jeden posiłek do syta, a od 14 wszyscy wstrzęmięźliwość od pokarmów mięsnych',
 'W dzisiejszą niedzielę Msza w intencji Żywego Różańca i Parafian o 8:30. Po niej Adoracja Eucharystyczna i zmiana tajemnic.',
-'W dzisiejszą niedzielę o 15:00 zapraszam członków naszych rad parafialnych na spotkanie na plebanię.'
+'W dzisiejszą niedzielę o 15:00 zapraszam członków naszych rad parafialnych na spotkanie na plebanię.',
 'W najbliższy piątek Pierwszy piątek miesiąca. Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00.',
-" W następną niedzielę wizytacja kanoniczna. Przywitanie Biskupa Janusza Ostrowskiego o 8.30 i sprawozdanie, po Mszy nawiedzenie cmentarza i wizyta u ww Domu Zakonnym Sióstr Katarzynek. O 11.00 Suma, a po niej Gorzkie Żale. Potem na plebanii spotkanie z radami parafialnymi i odpowiedzialnymi za wspólnoty.'];
+'W następną niedzielę wizytacja kanoniczna. Przywitanie Biskupa Janusza Ostrowskiego o 8.30 i sprawozdanie, po Mszy nawiedzenie cmentarza i wizyta u ww Domu Zakonnym Sióstr Katarzynek. O 11.00 Suma, a po niej Gorzkie Żale. Potem na plebanii spotkanie z radami parafialnymi i odpowiedzialnymi za wspólnoty.',];
