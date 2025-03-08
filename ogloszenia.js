df358268a236bf7aeea3945e23d117e5604226aa
mproszek <mproszek@op.pl>
2025-03-08 20:51:37 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,7 +1,6 @@
-var ogloszeniaNaglowek = '8 NIEDZIELA ZWYKŁA C - 2.03.2025 R.';
+var ogloszeniaNaglowek = '1 NIEDZIELA WIELKIEGO POSTU C - 9.03.2025 R.';
 var ogloszenia = [
-    'W najbliższą środę - Środa Popielcowa. Początek Wielkiego Postu. Msze z posypaniem głów popiołem o 11.00 i 18.00. Jest to dzień postu ścisłęgo. Osoby dorosłe od 18 r. do 60 jeden posiłek do syta, a od 14 wszyscy wstrzęmięźliwość od pokarmów mięsnych',
-'W dzisiejszą niedzielę Msza w intencji Żywego Różańca i Parafian o 8:30. Po niej Adoracja Eucharystyczna i zmiana tajemnic.',
-'W dzisiejszą niedzielę o 15:00 zapraszam członków naszych rad parafialnych na spotkanie na plebanię.',
-'W najbliższy piątek Pierwszy piątek miesiąca. Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00.',
-'W następną niedzielę wizytacja kanoniczna. Przywitanie Biskupa Janusza Ostrowskiego o 8.30 i sprawozdanie, po Mszy nawiedzenie cmentarza i wizyta u ww Domu Zakonnym Sióstr Katarzynek. O 11.00 Suma, a po niej Gorzkie Żale. Potem na plebanii spotkanie z radami parafialnymi i odpowiedzialnymi za wspólnoty.',];
+    'Bóg zapłać Wszystkim parafianom za przygotowanie dzisiejszej wizytacj, kaplic kalwarii przed Drogami Krzyżowymi i miejsc przy kościele przed nabożeństwami wielkopostnymi',
+'W następną niedzielę modlimy się za misjonarzy i ofiary przeznaczamy na misje',
+'W najbliższy piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00.',
+'Dziś po Mszy o 8.30 w ramach wizytacji kanonicznej wraz z ks. Biskupem odbędzie się nawiedzenie cmentarzy i wizyta u w Domu Zakonnym Sióstr Katarzynek. O 11.00 Suma, a po niej Gorzkie Żale. Potem na plebanii spotkanie z radami parafialnymi i odpowiedzialnymi za wspólnoty.',];
