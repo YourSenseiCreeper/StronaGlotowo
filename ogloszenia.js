3d1adf098835f2787d124dfdd8db655183337143
mproszek <mproszek@op.pl>
2025-03-15 19:50:56 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,6 +1,7 @@
-var ogloszeniaNaglowek = '1 NIEDZIELA WIELKIEGO POSTU C - 9.03.2025 R.';
+var ogloszeniaNaglowek = '2 NIEDZIELA WIELKIEGO POSTU C - 16.03.2025 R.';
 var ogloszenia = [
-    'Bóg zapłać Wszystkim parafianom za przygotowanie dzisiejszej wizytacj, kaplic kalwarii przed Drogami Krzyżowymi i miejsc przy kościele przed nabożeństwami wielkopostnymi',
-'W następną niedzielę modlimy się za misjonarzy i ofiary przeznaczamy na misje',
+    'Bóg zapłać Wszystkim za dzisiejsze ofiary przeznacze na misje, w przyszłą niedzielę ofiary na fundusz remontowy.',
 'W najbliższy piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00.',
-'Dziś po Mszy o 8.30 w ramach wizytacji kanonicznej wraz z ks. Biskupem odbędzie się nawiedzenie cmentarzy i wizyta u w Domu Zakonnym Sióstr Katarzynek. O 11.00 Suma, a po niej Gorzkie Żale. Potem na plebanii spotkanie z radami parafialnymi i odpowiedzialnymi za wspólnoty.',];
+'Spotkanie dla rodziców i młodzieży przed Bierzmowaniem za tydzień po Mszy o 11.00 i po Gorzkich Żalach.',
+'W przedsionku jest wywieszona informacja o rekolekcjach oazowych dla dzieci i młodzieży w Nowym Kawkowie. ',
+];
