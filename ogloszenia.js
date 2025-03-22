cb8079f1f10709ce10f4688d942e1c11c573123a
mproszek <mproszek@op.pl>
2025-03-22 22:47:55 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,7 +1,8 @@
-var ogloszeniaNaglowek = '2 NIEDZIELA WIELKIEGO POSTU C - 16.03.2025 R.';
+var ogloszeniaNaglowek = '3 NIEDZIELA WIELKIEGO POSTU C - 23.03.2025 R.';
 var ogloszenia = [
-    'Bóg zapłać Wszystkim za dzisiejsze ofiary przeznacze na misje, w przyszłą niedzielę ofiary na fundusz remontowy.',
-'W najbliższy piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00.',
-'Spotkanie dla rodziców i młodzieży przed Bierzmowaniem za tydzień po Mszy o 11.00 i po Gorzkich Żalach.',
-'W przedsionku jest wywieszona informacja o rekolekcjach oazowych dla dzieci i młodzieży w Nowym Kawkowie. ',
+    'Bóg zapłać Wszystkim za dzisiejsze ofiary przeznacze na fundusz remontowy.',
+'W najbliższy piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00, którą poprowadzą klerycy naszego Seminarium.',
+'Dziś spotkanie dla rodziców i młodzieży przed Bierzmowaniem po Mszy o 11.00 i po Gorzkich Żalach, a za tydzień po Sumie i Gorzkich Żalach spotkanie rodziców i dzieci prze I Komunią.',
+'Do sakramentu małżeństwa przygotowują się Marzena Śliwka, panna z Głotowa i Michał Enerlich, kawaler z Wilczkowa. Narzeczonych obejmujemy modlitwami, a kto wiedziałby o zaistniałych przeszkodach powinien powiadomić urząd parafialny. ',
+'Ofiary na kwiaty do Grobu Pańskiego można składać do puszki na ołtarzu Matki Bożej.',
 ];
