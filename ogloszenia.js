6b0ab8c1feb777297306f634ac39afc897f9a7be
mproszek <mproszek@op.pl>
2025-03-29 21:02:33 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,8 @@
-var ogloszeniaNaglowek = '3 NIEDZIELA WIELKIEGO POSTU C - 23.03.2025 R.';
+var ogloszeniaNaglowek = '4 NIEDZIELA WIELKIEGO POSTU C - 30.03.2025 R.';
 var ogloszenia = [
-    'Bóg zapłać Wszystkim za dzisiejsze ofiary przeznacze na fundusz remontowy.',
-'W najbliższy piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00, którą poprowadzą klerycy naszego Seminarium.',
-'Dziś spotkanie dla rodziców i młodzieży przed Bierzmowaniem po Mszy o 11.00 i po Gorzkich Żalach, a za tydzień po Sumie i Gorzkich Żalach spotkanie rodziców i dzieci prze I Komunią.',
+ 
+'Zapraszam na Godziny Miłoseirdza Bożego w piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00, którą poprowadzą klerycy naszego Seminarium.',
+'Dziś spotkanie po Sumie i Gorzkich Żalach spotkanie rodziców i dzieci prze I Komunią.',
 'Do sakramentu małżeństwa przygotowują się Marzena Śliwka, panna z Głotowa i Michał Enerlich, kawaler z Wilczkowa. Narzeczonych obejmujemy modlitwami, a kto wiedziałby o zaistniałych przeszkodach powinien powiadomić urząd parafialny. ',
 'Ofiary na kwiaty do Grobu Pańskiego można składać do puszki na ołtarzu Matki Bożej.',
 ];
