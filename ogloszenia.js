9be049b0715c2ded8b7785804b64005d197ea802
mproszek <mproszek@op.pl>
2025-04-06 00:12:42 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,8 @@
-var ogloszeniaNaglowek = '4 NIEDZIELA WIELKIEGO POSTU C - 30.03.2025 R.';
+var ogloszeniaNaglowek = '5 NIEDZIELA WIELKIEGO POSTU C - 6.04.2025 R.';
 var ogloszenia = [
  
-'Zapraszam na Godziny Miłoseirdza Bożego w piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00, którą poprowadzą klerycy naszego Seminarium.',
-'Dziś spotkanie po Sumie i Gorzkich Żalach spotkanie rodziców i dzieci prze I Komunią.',
-'Do sakramentu małżeństwa przygotowują się Marzena Śliwka, panna z Głotowa i Michał Enerlich, kawaler z Wilczkowa. Narzeczonych obejmujemy modlitwami, a kto wiedziałby o zaistniałych przeszkodach powinien powiadomić urząd parafialny. ',
+'Zapraszam na Godziny Miłoseirdza Bożego w piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00 w tym tygodniu rekolekcyjna.',
+'Jutro Msza o 9.00. W następną  niedzielę - Uroczystość Męki Pańskiej - N. Palmowa. Msze z poświęceniem palm i procesją o 8.30 i 11.00',
+'Nasze rekoldekcje wielkopostne będziemy przeżywać w najbliższy piątek i sobotę. Msze z okazją do spowiedzi będą o 11.00 z udziałem dzieci szkolnych i 18.00.W piątek od 13.00 odwiedziny chorych, których można zgłaszać. Rekolekcje poprowadzi ks. prob. Bogumił Wykowski z Biesala. Ofiary w sobotę zbieramy na dzieła rekolekcjonisty. Proszę o dary na stół odpustowy.',
 'Ofiary na kwiaty do Grobu Pańskiego można składać do puszki na ołtarzu Matki Bożej.',
 ];
