d847393c19dba79228b7ba2136d984516f1daa3a
mproszek <mproszek@op.pl>
2025-04-12 22:01:05 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,11 @@
-var ogloszeniaNaglowek = '5 NIEDZIELA WIELKIEGO POSTU C - 6.04.2025 R.';
+var ogloszeniaNaglowek = 'NIEDZIELA PALMOWA C - 13.04.2025 R.';
 var ogloszenia = [
  
-'Zapraszam na Godziny Miłoseirdza Bożego w piątek Msza z nabożeństwem i zmodlitwą o uzdrowienie duszy i ciała o 18.00. Droga Krzyżowa na Kalwarii o 17.00 w tym tygodniu rekolekcyjna.',
-'Jutro Msza o 9.00. W następną  niedzielę - Uroczystość Męki Pańskiej - N. Palmowa. Msze z poświęceniem palm i procesją o 8.30 i 11.00',
-'Nasze rekoldekcje wielkopostne będziemy przeżywać w najbliższy piątek i sobotę. Msze z okazją do spowiedzi będą o 11.00 z udziałem dzieci szkolnych i 18.00.W piątek od 13.00 odwiedziny chorych, których można zgłaszać. Rekolekcje poprowadzi ks. prob. Bogumił Wykowski z Biesala. Ofiary w sobotę zbieramy na dzieła rekolekcjonisty. Proszę o dary na stół odpustowy.',
+'Bóg zapłać za przygotowanie rekolekcji w kuchni, w liturgii, w kościele i za dary na stół odpustowy. Bóg zapłać naszej szkole za pomoc i udział.',
+'Dziś Uroczystość Męki Pańskiej - N. Palmowa. Msze z poświęceniem palm i procesją o 8.30 i 11.00, po Sumie Gorzkie Żale',
+'W tym tygodniu będziemy uczestniczyć w Triduum Paschalnym. W wielki Czwartek Msza Wieczerzy Pańskiej o 18.00, po niej początek adoracji P. Jezusa w ciemnicy. W wielki Piątek adoracja P. Jezusa w Ciemnicy od 10.00. Zapraszam poszczególne osoby od 10-11 z ul. Szkolnej, od 11-12 z ul Kościelnej, 12-13 z kol. pd, 13-14 z kol pn, 14-15 Swobodna, o 17.00 Droga Krzyżowa na Kalwarii, po niej Liturgia Męki Pańskiej i Adoracja P. Jezusa w Grobie do 21.',
+'W Wielką Sobotę adoracja P. Jezusa w grobie od 10. Zapraszam poszczególne osoby od 10-11 z ul. Szkolnej, od 11-12 z ul Kościelnej, 12-13 z kol. pd, 13-14 z kol pn, 14-15 Swobodna, o 18.00 Liturgia Wigilii Paschalnej z odnowieniem ze świecami przyrzeczeń chrzcielnych, po niej adoracja P. Jezusa w grobie do 21 . Poświęcenie pokarmów wielkanocnych w Swobodnie o 11, w Głotowie o 12 i po Wigilii Paschalnej. ',
+'W Niedzielę Zmartwychwstania Rezurekcje z procesją o 6.00 i Suma Wielkanocna o 11.00. ',
+' Zapraszam do przygotowania Grobu i Ciemnicy strażaków i chętnych we wtorek o 16.00. Wtedy też egzamin dla kandydatów do Bierzmowania.  Zapraszam dzieci, młodzież i dorosłych a szczególnie Strażaków do przygotowania adoracji i procesji.',
 'Ofiary na kwiaty do Grobu Pańskiego można składać do puszki na ołtarzu Matki Bożej.',
 ];
