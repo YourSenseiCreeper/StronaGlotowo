01b7baf1efb1784bd40654e951e4001193df60d7
mproszek <mproszek@op.pl>
2025-04-19 16:46:22 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,11 +1,9 @@
-var ogloszeniaNaglowek = 'NIEDZIELA PALMOWA C - 13.04.2025 R.';
+var ogloszeniaNaglowek = 'NIEDZIELA WIELKANOCNA C - 20.04.2025 R.';
 var ogloszenia = [
  
-'Bóg zapłać za przygotowanie rekolekcji w kuchni, w liturgii, w kościele i za dary na stół odpustowy. Bóg zapłać naszej szkole za pomoc i udział.',
-'Dziś Uroczystość Męki Pańskiej - N. Palmowa. Msze z poświęceniem palm i procesją o 8.30 i 11.00, po Sumie Gorzkie Żale',
-'W tym tygodniu będziemy uczestniczyć w Triduum Paschalnym. W wielki Czwartek Msza Wieczerzy Pańskiej o 18.00, po niej początek adoracji P. Jezusa w ciemnicy. W wielki Piątek adoracja P. Jezusa w Ciemnicy od 10.00. Zapraszam poszczególne osoby od 10-11 z ul. Szkolnej, od 11-12 z ul Kościelnej, 12-13 z kol. pd, 13-14 z kol pn, 14-15 Swobodna, o 17.00 Droga Krzyżowa na Kalwarii, po niej Liturgia Męki Pańskiej i Adoracja P. Jezusa w Grobie do 21.',
-'W Wielką Sobotę adoracja P. Jezusa w Grobie od 10. Zapraszam poszczególne osoby od 10-11 z ul. Szkolnej, od 11-12 z ul Kościelnej, 12-13 z kol. pd, 13-14 z kol pn, 14-15 Swobodna, o 18.00 Liturgia Wigilii Paschalnej z odnowieniem ze świecami przyrzeczeń chrzcielnych, po niej adoracja P. Jezusa w grobie do 21 . Poświęcenie pokarmów wielkanocnych w Swobodnie o 11, w Głotowie o 12 i po Wigilii Paschalnej. ',
-'W Niedzielę Zmartwychwstania Rezurekcje z procesją o 6.00 i Suma Wielkanocna o 11.00. ',
-'Zapraszam do przygotowania Grobu i Ciemnicy strażaków i chętnych we wtorek o 16.00. Wtedy też egzamin dla kandydatów do Bierzmowania.Zapraszam dzieci, młodzież i dorosłych a szczególnie Strażaków do przygotowania adoracji i procesji.',
-'Ofiary na kwiaty do Grobu Pańskiego można składać do puszki na ołtarzu Matki Bożej.',
+'Bóg zapłać za przygotowanie uroczystości Wielkanocnych wszystkim za ofiary na kwiaty, przygotowującym grób i dekorację, sponsorom dywanu, krzyża ołtarzowego, naczyń na oleje i alby oraz wszelkich ofiar, służbie liturgicznej, Strażakom, uczestnikom procesji.',
+'Adoracja P. Jezusa w grobie do 21. ',
+'W Niedzielę Zmartwychwstania Rezurekcje z procesją o 6.00 i Suma Wielkanocna o 11.00.W 2 Dzień Świąt Msze o 8.30 i 11.00. Ofiary na KUL. ',
+'W najbliższy piątek o 17.00 Bierzmowanie młodzieży, w czwartek spowiedź młodzieży, świadków i rodzin, we wtorek egzamin kurialny o 16.00 i próba, w środę sprzątanie kościoła od 16.00 i próba.',
+'W następną Niedzielę Miłosierdzia ofiary na Caritas.',
 ];
