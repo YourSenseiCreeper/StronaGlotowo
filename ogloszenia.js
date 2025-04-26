857ca22a6dd2cae8793da70ea04bd6f07c00cb18
mproszek <mproszek@op.pl>
2025-04-27 00:37:35 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,10 @@
-var ogloszeniaNaglowek = 'NIEDZIELA WIELKANOCNA C - 20.04.2025 R.';
+var ogloszeniaNaglowek = 'NIEDZIELA MIŁOSIERDZIA C - 27.04.2025 R.';
 var ogloszenia = [
  
-'Bóg zapłać za przygotowanie uroczystości Wielkanocnych wszystkim za ofiary na kwiaty, przygotowującym grób i dekorację, sponsorom dywanu, krzyża ołtarzowego, naczyń na oleje i alby oraz wszelkich ofiar, służbie liturgicznej, Strażakom, uczestnikom procesji.',
-'Adoracja P. Jezusa w grobie do 21. ',
-'W Niedzielę Zmartwychwstania Rezurekcje z procesją o 6.00 i Suma Wielkanocna o 11.00.W 2 Dzień Świąt Msze o 8.30 i 11.00. Ofiary na KUL. ',
-'W najbliższy piątek o 17.00 Bierzmowanie młodzieży, w czwartek spowiedź młodzieży, świadków i rodzin, we wtorek egzamin kurialny o 16.00 i próba, w środę sprzątanie kościoła od 16.00 i próba.',
-'W następną Niedzielę Miłosierdzia ofiary na Caritas.',
+'Zapraszam Strażaków i wszystkich chętnych, aby zdemontować grób i ciemnicę jutro od godz. 16.00 .',
+'1.05. rozpoczynamy nabożeństwa majowe po Mszy o 18.00. ',
+'W I piątek miesiąca Msza o 16.00 po niej nabożeństwo.. ',
+'3,05. Uroczystość NMP Królowej Polski - Msze o 8.30 i 11.00- modlimy się za Strażaków, po niej nabożeństwo majowe.',
+'Bóg zapłać za ofiary dzisiejsze na Caritas.',
+'W następną niedzielę Msza o 8.30 w intencji Żywego Różańca i Parafian, po niej adoracja Eucharystyczna, zmiana tajemnic i nabożeństwo majowe.',
 ];
