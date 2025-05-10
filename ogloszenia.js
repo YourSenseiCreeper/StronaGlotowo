6fc07d0f532385c1c9d21d1bb09b4f6095e8ef06
mproszek <mproszek@op.pl>
2025-05-10 20:15:32 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,10 +1,10 @@
-var ogloszeniaNaglowek = 'NIEDZIELA MIŁOSIERDZIA C - 27.04.2025 R.';
+var ogloszeniaNaglowek = '4 NIEDZIELA WIELKANOCNA C - 11.05.2025 R.';
 var ogloszenia = [
  
-'Zapraszam Strażaków i wszystkich chętnych, aby zdemontować grób i ciemnicę jutro od godz. 16.00 .',
-'1.05. rozpoczynamy nabożeństwa majowe po Mszy o 18.00. ',
-'W I piątek miesiąca Msza o 16.00 po niej nabożeństwo.. ',
+'W najbiższą sobotę 17.05. będziemy przeżywać 131 Rocznicę Poświęcenia Kalwarii. Uroczystość rozpocznie się o 9.30 wprowadzeniem i koncertem PRO MUSICA ANTIQUA, o 11.00 Msza - przewodniczy Ks. ABp Józef Górzyński, Metrpolita Warmiński, po niej spotkanie przy Krzyżu Solidarności , a potem ok.12.30 Droga Krzyżowa na Kalwarii. Zapraszam wszystkich , szczególnie opiekunów kaplic.',
+'Zapraszam na nabożeństwa majowe po Mszy o 18.00, a dziś po Sumie. ',
+'W środę o 17.00 próba przed I Komunią, a w niedzielę po Mszy o 11.00 i nabożeństwie majowym spotkanie rodziców i dzieci przed I Komunią. ',
 '3,05. Uroczystość NMP Królowej Polski - Msze o 8.30 i 11.00- modlimy się za Strażaków, po niej nabożeństwo majowe.',
-'Bóg zapłać za ofiary dzisiejsze na Caritas.',
-'W następną niedzielę Msza o 8.30 w intencji Żywego Różańca i Parafian, po niej adoracja Eucharystyczna, zmiana tajemnic i nabożeństwo majowe.',
+'Do sakramentu małżeństwa przygotuwują się Michał Ofierski, kwaler ze Swobodna, Parafia Głotowo i Julita Jamróz , panna z Dobrego Miasta z para. Najświętszego Zbawiciela i wszystkich /Św. Narzeczonych obejmujemy modlitwami a kto wiedziałby o zaistniałych przeszkodach powinien powiadomić urząd parafialny.',
+
 ];
