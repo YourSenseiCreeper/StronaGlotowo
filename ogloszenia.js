5e73e01206ed4c138e40c4da881f5d9094d946f6
mproszek <mproszek@op.pl>
2025-06-01 00:05:50 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,10 +1,10 @@
-var ogloszeniaNaglowek = '6 NIEDZIELA WIELKANOCNA C - 25.05.2025 R.';
+var ogloszeniaNaglowek = '7 NIEDZIELA WIELKANOCNA - WNIEBOWSTĄPIENIE C - 1.06.2025 R.';
 var ogloszenia = [
  
 'Ogłoszenie o Beatyfikacji.',
-'Zapraszam na nabożeństwa majowe po Mszy o 18.00, a dziś po Sumie. ',
-'W środę o 17.00 próba przed I Komunią. ',
-'W następną niedzielę Msza w intecji Róż Różańcowych i parafian o 8.30, po niej Adoracja Eucharystyczna, zmiana tajemnic i nabożeństwo czerwcowe..',
-'Do sakramentu małżeństwa przygotuwują się Michał Ofierski, kwaler ze Swobodna, Parafia Głotowo i Julita Jamróz , panna z Dobrego Miasta z para. Najświętszego Zbawiciela i wszystkich /Św. Narzeczonych obejmujemy modlitwami a kto wiedziałby o zaistniałych przeszkodach powinien powiadomić urząd parafialny.',
+'Zapraszam na nabożeństwa czerwcowe po Mszy o 18.00, a dziś po Mszy o 8.30. gdy rozpocznie się Adoracja Eucharystyczna, zaśpiewamy czerwcowe w tym czasie adoracja relikwi bł. kard Wyszyńskiego, a potem zmiana tajemnic. ',
+'W środę o 17.15 próba przed I Komunią. ',
+'W tym tygodniu wypada I piatek i I sobota miesiąca. Msze z nabożeństwem o 18.00.',
+'Zapraszam członków rady duszpasterskiej na spotkanie dziś o 15.00 na plebanii.',
 
 ];
