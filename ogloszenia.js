243bd7b1af2b09f4ef72f336b4c055fdc8561dde
mproszek <mproszek@op.pl>
2025-06-08 00:04:57 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,10 +1,9 @@
-var ogloszeniaNaglowek = '7 NIEDZIELA WIELKANOCNA - WNIEBOWSTĄPIENIE C - 1.06.2025 R.';
+var ogloszeniaNaglowek = ' NIEDZIELA ZESŁANIA DUCHA ŚW. C - 8.06.2025 R.';
 var ogloszenia = [
  
-'Ogłoszenie o Beatyfikacji.',
-'Zapraszam na nabożeństwa czerwcowe po Mszy o 18.00, a dziś po Mszy o 8.30. gdy rozpocznie się Adoracja Eucharystyczna, zaśpiewamy czerwcowe w tym czasie adoracja relikwi bł. kard Wyszyńskiego, a potem zmiana tajemnic. ',
-'W środę o 17.15 próba przed I Komunią. ',
-'W tym tygodniu wypada I piatek i I sobota miesiąca. Msze z nabożeństwem o 18.00.',
-'Zapraszam członków rady duszpasterskiej na spotkanie dziś o 15.00 na plebanii.',
+'Zapraszam na nabożeństwa czerwcowe po Mszy o 18.00, a dziś po Sumie.',
+'W środę o 17.00 próba generalna przed I Komunią.',
+'W sobotę spowież dla dzieci kumunijnych i ich rodzin o 10.00.',
+'W następną niedzielę będziemy przeżywać I Komunię dzieci na Mszy o 11.00.',
 
 ];
