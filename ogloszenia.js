28e8b3b77f0e56548fd8533ef0ed0fa98aecd4c4
mproszek <mproszek@op.pl>
2025-06-15 00:21:30 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -5,5 +5,5 @@ var ogloszenia = [
 'W czwartek Uroczystość Bożego Ciała. Msza o 11.00 i po niej procesja. Proszę o przygotowanie kaplic Bożego Ciała do uroczystości',
 'W następną niedzielę nasz Odpust. Msza o 8.30, o 10.00 Droga Krzyżowa na Kalwarii, o 11.00 - Adoracja i różaniec w kościele, o 12.00 Procesja i Suma Odpustowa - przewodniczy ks. Profesor Mirosłąw Hulecki z Olsztyna. Proszę o przygotowanie, kaplic, procesji i dary na stół odpustowy .',
 'Bóg zapłąć wszystkim za przygotowanie dzisiejszej uroczystości, za przygotowanie kaplic i kwiatów, za koszenie dla p. Zdzisława i OSP oraz za upiększanie i ukwiecenie wsi dla P. Sołtys i wszystkich wspomagających, p. Heleny i p. Wojtka.',
-
+'Do sakramentu małżęństwa przygotowują się Paweł Szczurko, kawaler z Głotowa i Klaudia Niedżwiedzka, panna z Głotowa. Narzeczonych obejmujemy modlitwą , a kto wiedziałby o zaistnieniu przeszkód w zawarciu małżeństwa powinien powiadomić urząd parafialny.',
 ];
