a640f05341b211bfeb1123527b4ee327e0fa0c53
mproszek <mproszek@op.pl>
2025-06-21 23:38:51 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,9 @@
-var ogloszeniaNaglowek = ' NIEDZIELA TRÓJCY ŚW. C - 15.06.2025 R.';
+var ogloszeniaNaglowek = ' NIEDZIELA ODPUSTOWA C - 15.06.2025 R.';
 var ogloszenia = [
  
-'Zapraszam na nabożeństwa czerwcowe po Mszy o 18.00, a dziś po Sumie, na której 10 dzieci przyjmie I Komunię',
-'W czwartek Uroczystość Bożego Ciała. Msza o 11.00 i po niej procesja. Proszę o przygotowanie kaplic Bożego Ciała do uroczystości',
-'W następną niedzielę nasz Odpust. Msza o 8.30, o 10.00 Droga Krzyżowa na Kalwarii, o 11.00 - Adoracja i różaniec w kościele, o 12.00 Procesja i Suma Odpustowa - przewodniczy ks. Profesor Mirosłąw Hulecki z Olsztyna. Proszę o przygotowanie, kaplic, procesji i dary na stół odpustowy .',
-'Bóg zapłąć wszystkim za przygotowanie dzisiejszej uroczystości, za przygotowanie kaplic i kwiatów, za koszenie dla p. Zdzisława i OSP oraz za upiększanie i ukwiecenie wsi dla P. Sołtys i wszystkich wspomagających, p. Heleny i p. Wojtka.',
+'Zapraszam na nabożeństwa czerwcowe po Mszy o 18.00, a dziś w czasie procesji',
+'W czwartek zakończenie roku szkolnego Mszą o 9.00.',
+'Dziś w niedzielę nasz Odpust. Msza o 8.30, o 10.00 Droga Krzyżowa na Kalwarii, o 11.00 - Adoracja i różaniec w kościele, o 12.00 Procesja i Suma Odpustowa - przewodniczy ks. Profesor Mirosław Hulecki z Olsztyna.Po procesji adoracja relikwi bł. Carlo Acutisa Proszę odebranie obrazków bł. Carlo Acutisa, zamówienie intencji czy nabycie pamiątek przy kaplicy XIV .',
+'Bóg zapłąć wszystkim za przygotowanie dzisiejszej uroczystości,kapłanom, służbie liturgicznej, ks. Adamowi, uczestnikom procesji, OSP, Canzonetcie, wszystkim przygotowującym i pomagającym w tej uroczystości.',
 'Do sakramentu małżęństwa przygotowują się Paweł Szczurko, kawaler z Głotowa i Klaudia Niedżwiedzka, panna z Głotowa. Narzeczonych obejmujemy modlitwą , a kto wiedziałby o zaistnieniu przeszkód w zawarciu małżeństwa powinien powiadomić urząd parafialny.',
 ];
