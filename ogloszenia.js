62a61929d4b43ca5a68353f7e41c5f45c9a41b77
mproszek <mproszek@op.pl>
2025-07-19 20:11:18 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,7 @@
-var ogloszeniaNaglowek = ' NIEDZIELA ODPUSTOWA C - 15.06.2025 R.';
+var ogloszeniaNaglowek = '16 NIEDZIELA ZWYKŁA C - 20.07.2025 R.';
 var ogloszenia = [
  
-'Zapraszam na nabożeństwa czerwcowe po Mszy o 18.00, a dziś w czasie procesji',
-'W czwartek zakończenie roku szkolnego Mszą o 9.00.',
-'Dziś w niedzielę nasz Odpust. Msza o 8.30, o 10.00 Droga Krzyżowa na Kalwarii, o 11.00 - Adoracja i różaniec w kościele, o 12.00 Procesja i Suma Odpustowa - przewodniczy ks. Profesor Mirosław Hulecki z Olsztyna.Po procesji adoracja relikwi bł. Carlo Acutisa Proszę odebranie obrazków bł. Carlo Acutisa, zamówienie intencji czy nabycie pamiątek przy kaplicy XIV .',
-'Bóg zapłąć wszystkim za przygotowanie dzisiejszej uroczystości,kapłanom, służbie liturgicznej, ks. Adamowi, uczestnikom procesji, OSP, Canzonetcie, wszystkim przygotowującym i pomagającym w tej uroczystości.',
-'Do sakramentu małżęństwa przygotowują się Paweł Szczurko, kawaler z Głotowa i Klaudia Niedżwiedzka, panna z Głotowa. Narzeczonych obejmujemy modlitwą , a kto wiedziałby o zaistnieniu przeszkód w zawarciu małżeństwa powinien powiadomić urząd parafialny.',
+'Bóg zapłać za ofiary na fundusz remontowy i osobom, które przygotowały kwiaty do kościoła',
+'W przyszłą niedzielę po Mszach modlitwa za wstawiennictwem św. Krzysztofa i poświęcenie pojazdów. Ofiary na zakup pojazdów dla misjonarzy.',
+
 ];
