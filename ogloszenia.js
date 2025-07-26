7355d6454b62aa817f1d6ee52a9670c24eb02f32
mproszek <mproszek@op.pl>
2025-07-26 23:40:58 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -4,7 +4,7 @@ var ogloszenia = [
 'Bóg zapłać za ofiary na fundusz na pojazdy dla misjonarzy i osobom, które przygotowały kwiaty do kościoła',
 'Dziś w niedzielę po Mszach modlitwa za wstawiennictwem św. Krzysztofa i poświęcenie pojazdów.',
 'w tym tygodniu wypada I piątek i I sobota miesiąca.Msze z nabożeństwami będą o 18.00.',
-'W przyszłą niedzielę Msza w i tencj członków Żywego Różańca i Parafian o 8.30,po niej Adoracja Eucharystyczna izmiana tajemnic.'
-'Można się zapisać na pielgrzymkę autokarową śladami miłosierdzia w dniach 18-23 sierpnia. Szczegóły na tablicy w przedsionku kościoła.' 
+'W przyszłą niedzielę Msza w i tencj członków Żywego Różańca i Parafian o 8.30,po niej Adoracja Eucharystyczna izmiana tajemnic.',
+'Można się zapisać na pielgrzymkę autokarową śladami miłosierdzia w dniach 18-23 sierpnia. Szczegóły na tablicy w przedsionku kościoła.', 
 
 ];
