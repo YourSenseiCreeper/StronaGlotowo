b93de9ad0ee935061b189dad34930fb42e19144d
mproszek <mproszek@op.pl>
2025-07-26 23:44:38 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,7 +1,7 @@
 var ogloszeniaNaglowek = '17 NIEDZIELA ZWYKŁA C - 27.07.2025 R.';
 var ogloszenia = [
  
-'Bóg zapłać za ofiary na fundusz na pojazdy dla misjonarzy i osobom, które przygotowały kwiaty do kościoła',
+'Bóg zapłać za ofiary na fundusz na pojazdy dla misjonarzy i osobom, które przygotowały kwiaty do kościoła.',
 'Dziś w niedzielę po Mszach modlitwa za wstawiennictwem św. Krzysztofa i poświęcenie pojazdów.',
 'w tym tygodniu wypada I piątek i I sobota miesiąca.Msze z nabożeństwami będą o 18.00.',
 'W przyszłą niedzielę Msza w i tencj członków Żywego Różańca i Parafian o 8.30,po niej Adoracja Eucharystyczna izmiana tajemnic.',
