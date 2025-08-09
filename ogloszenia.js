c44d73fe98df6b9bccd407d26ea84af8632b43e3
mproszek <mproszek@op.pl>
2025-08-09 23:53:57 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,10 +1,9 @@
-var ogloszeniaNaglowek = '17 NIEDZIELA ZWYKŁA C - 27.07.2025 R.';
+var ogloszeniaNaglowek = '19 NIEDZIELA ZWYKŁA C - 10.08.2025 R.';
 var ogloszenia = [
  
-'Bóg zapłać za ofiary na fundusz na pojazdy dla misjonarzy i osobom, które przygotowały kwiaty do kościoła.',
-'Dziś w niedzielę po Mszach modlitwa za wstawiennictwem św. Krzysztofa i poświęcenie pojazdów.',
-'w tym tygodniu wypada I piątek i I sobota miesiąca.Msze z nabożeństwami będą o 18.00.',
-'W przyszłą niedzielę Msza w i tencj członków Żywego Różańca i Parafian o 8.30,po niej Adoracja Eucharystyczna izmiana tajemnic.',
+'Bógzapłać za dostaeczenie i ułożenie kwiatów w kościele. Można je przynosić nadal.',
+'15.08. w piątek uroczystość Wniebowzięcia NMP. Msze z poświęceniem ziół, roślin i kwiatów o 8.30 i 11.00.',
+'We wtorek 12.08. na plebaniiodbędzie się wystawa poplenerowa obrazów namalowanych w czasie pleneru. .',
 'Można się zapisać na pielgrzymkę autokarową śladami miłosierdzia w dniach 18-23 sierpnia. Szczegóły na tablicy w przedsionku kościoła.', 
 
 ];
