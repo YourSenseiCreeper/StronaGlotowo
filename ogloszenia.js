333bde97afac265a1d5c7c772b41c01b4f316a1f
mproszek <mproszek@op.pl>
2025-08-30 17:11:13 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,8 @@
-var ogloszeniaNaglowek = '19 NIEDZIELA ZWYKŁA C - 10.08.2025 R.';
+var ogloszeniaNaglowek = '22 NIEDZIELA ZWYKŁA C - 31.08.2025 R.';
 var ogloszenia = [
  
-'Bógzapłać za dostaeczenie i ułożenie kwiatów w kościele. Można je przynosić nadal.',
-'15.08. w piątek uroczystość Wniebowzięcia NMP. Msze z poświęceniem ziół, roślin i kwiatów o 8.30 i 11.00.',
-'We wtorek 12.08. na plebaniiodbędzie się wystawa poplenerowa obrazów namalowanych w czasie pleneru. .',
-'Można się zapisać na pielgrzymkę autokarową śladami miłosierdzia w dniach 18-23 sierpnia. Szczegóły na tablicy w przedsionku kościoła.', 
+'Jutro Msza na rozpoczęcie roku szkolnego o 9.00. Przed Mszą okazja do spowiedzi.',
+'W tym tygodniu wypada I piątek - Msza z nabożeństwem o 18.00, w  I sobotę Msza z nabożeństwem o 17.00.',
+'W następną niedzielę Msza w intencji członków Róż Różańcowych i Parafian o 8.30, po niej Adoracja Eucharystyczna i zmiana tajemnic różańca. O 15 na plebanii spotkanie członków naszych rad parafialnych.', 
 
 ];
