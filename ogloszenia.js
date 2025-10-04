1f2f3d4cbc16c52a38d312aac7894b8100151497
mproszek <mproszek@op.pl>
2025-10-04 22:53:03 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,9 @@
-var ogloszeniaNaglowek = 'NIEDZIELA ODPUSTOWA C - 21.09.2025 R.';
+var ogloszeniaNaglowek = '27 NIEDZIELA ZWYKŁA C - 4.10.2025 R.';
 var ogloszenia = [
  
-' Dziś w niedzielę nasz Odpust. Msza o 8.30, o 10.00 Droga Krzyżowa na Kalwarii, o 11.00 - Adoracja i różaniec w kościele, o 12.00 Procesja i Suma Odpustowa - przewodniczy ks. Jarosław Kieł z Malborka.',
-'Przy kaplicy XIV można nabyć pamiątki i zamówić intencje mszalne. Zapraszamy do hermbaciarni na zupę i ciastko, kawę i herbatę.',
-'Dziękuję wszystkim za przygotowanie odpustu kapłanom, klerykowi Adamowi, S. Iwonie, radzie parafialnej, chórowi, OSP, procesji i wszystkim , którzy przyczynili się do przygotowania i udziału w odpuście.', 
+' Dziś w niedzielę po Mszy o 8.30 adoracja eucharystyczna, zmiana tajemnic i nabożeństwo różańowe. Zapraszam na nabożeństwa różańcowe codziennie po Mszy o 18.00.',
+' Dziś po Mszy o 11.00 spotkanie dla rodziców i dzieci w ramach przygotowań do I Komunii.',
+' Dziękuję wszystkim za przygotowanie wczorajszych dożynek.',
+' W przyszłą niedzielę papieską ofiary składamy na Fundację dla młodzieży. Po Mszach adoracja relikwi św.Jana Pawła II. Po sumie nabożeństwo różańcowe.', 
 
 ];
