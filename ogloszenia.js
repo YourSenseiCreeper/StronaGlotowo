cfa53a59dd3dd235a9c74b29d5b7aafc4a33b1f9
mproszek <mproszek@op.pl>
2025-10-19 07:48:36 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,8 @@
-var ogloszeniaNaglowek = '28 NIEDZIELA ZWYKŁA C - 12.10.2025 R.';
+var ogloszeniaNaglowek = '29 NIEDZIELA ZWYKŁA C - 19.10.2025 R.';
 var ogloszenia = [
  
 ' Zapraszam na nabożeństwa różańcowe codziennie po Mszy o 18.00, Różaniec dla dzieci we wtorki i czwartki. a dziś po Sumie',
-' Parafia Świątki organizuje pielgrzymkę do Częstochowy w dniach 18-19.10. Można jeszcze się zgłosić. Informacje u proboszcza.',
-' Dziś w niedzielę papieską ofiary do puszki na ołtarzu Matki Bożej składamy na Fundację dla młodzieży. Po Mszach adoracja relikwi św.Jana Pawła II.', 
+' Na ołtarzu Matki Bożej są wyłożone kartki wypominkowe. Proszę o czytelne ich wypełnienie i zaznaczenie czy są to wypominki roczne, półroczne czy jednorazowe..',
+' Dziś w niedzielę po Mszach adoracja relikwi bł. ks. Jerzego Popiełuszki. Poświęcenie relikwiarza.', 
 
 ];
