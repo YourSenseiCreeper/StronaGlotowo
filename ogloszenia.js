d3f05bd03b6bb7383ff1b2df5d241b816f0e6cda
mproszek <mproszek@op.pl>
2025-10-25 22:36:21 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,10 @@
-var ogloszeniaNaglowek = '29 NIEDZIELA ZWYKŁA C - 19.10.2025 R.';
+var ogloszeniaNaglowek = '30 NIEDZIELA ZWYKŁA C - 26.10.2025 R.';
 var ogloszenia = [
  
 ' Zapraszam na nabożeństwa różańcowe codziennie po Mszy o 18.00, Różaniec dla dzieci we wtorki i czwartki. a dziś po Sumie',
 ' Na ołtarzu Matki Bożej są wyłożone kartki wypominkowe. Proszę o czytelne ich wypełnienie i zaznaczenie czy są to wypominki roczne, półroczne czy jednorazowe..',
-' Dziś w niedzielę po Mszach adoracja relikwi bł. ks. Jerzego Popiełuszki. Poświęcenie relikwiarza.', 
+' W I piątek Msza z nabożeństwem o 18.00 i zakończenie nabożeństw różańcowych. ', 
+' 1.11. Uroczystość Wszystkich Św. Msza o 11.00 z nabożeństwem pierwszosobotnim i o 13.00 - Msza Składkowa z wypominkami jednorazowymi, a po niej procesja na cmentarze.',
+' 2.11. w neiedzielę Dzień Zaduszny - o 8.30 Msza w intencji członkó Róż Różańcowych i parafian, po adoracja Eucharystyczna i nabożeństwo różańcowe z wypominkami, a po Mszy o 11.00 procesja na cmentarze.',
 
 ];
