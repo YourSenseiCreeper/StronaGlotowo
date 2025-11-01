2fefbc713f64dc7ace68fbe0dcd163585924a49d
mproszek <mproszek@op.pl>
2025-11-02 00:23:28 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,10 +1,9 @@
-var ogloszeniaNaglowek = '30 NIEDZIELA ZWYKŁA C - 26.10.2025 R.';
+var ogloszeniaNaglowek = '31 NIEDZIELA ZWYKŁA C - 2.11.2025 R.';
 var ogloszenia = [
  
-' Zapraszam na nabożeństwa różańcowe codziennie po Mszy o 18.00, Różaniec dla dzieci we wtorki i czwartki. a dziś po Sumie',
-' Na ołtarzu Matki Bożej są wyłożone kartki wypominkowe. Proszę o czytelne ich wypełnienie i zaznaczenie czy są to wypominki roczne, półroczne czy jednorazowe..',
-' W I piątek Msza z nabożeństwem o 18.00 i zakończenie nabożeństw różańcowych. ', 
-' 1.11. Uroczystość Wszystkich Św. Msza o 11.00 z nabożeństwem pierwszosobotnim i o 13.00 - Msza Składkowa z wypominkami jednorazowymi, a po niej procesja na cmentarze.',
+' Na ołtarzu Matki Bożej są wyłożone kartki wypominkowe. Proszę o czytelne ich wypełnienie i zaznaczenie czy są to wypominki roczne czy półroczne.',
+' W I piątek Msza z nabożeństwem o 18.00. ', 
+' Bóg zapłąć za ofiary na kwiaty do koscioła i za ofiary z okazji wypominek. Zaczęliśmy konserwację polichromii przedsionka wejściowego.',
 ' 2.11. w neiedzielę Dzień Zaduszny - o 8.30 Msza w intencji członkó Róż Różańcowych i parafian, po adoracja Eucharystyczna i nabożeństwo różańcowe z wypominkami, a po Mszy o 11.00 procesja na cmentarze.',
 
 ];
