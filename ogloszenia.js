f6cbc532c9e89daef6521354901496c9436c986b
mproszek <mproszek@op.pl>
2025-11-08 20:54:19 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,9 @@
-var ogloszeniaNaglowek = '31 NIEDZIELA ZWYKŁA C - 2.11.2025 R.';
+var ogloszeniaNaglowek = '32 NIEDZIELA ZWYKŁA C - 9.11.2025 R.';
 var ogloszenia = [
  
-' Na ołtarzu Matki Bożej są wyłożone kartki wypominkowe. Proszę o czytelne ich wypełnienie i zaznaczenie czy są to wypominki roczne czy półroczne.',
-' W I piątek Msza z nabożeństwem o 18.00. ', 
-' Bóg zapłąć za ofiary na kwiaty do koscioła i za ofiary z okazji wypominek. Zaczęliśmy konserwację polichromii przedsionka wejściowego.',
-' 2.11. w neiedzielę Dzień Zaduszny - o 8.30 Msza w intencji członkó Róż Różańcowych i parafian, po adoracja Eucharystyczna i nabożeństwo różańcowe z wypominkami, a po Mszy o 11.00 procesja na cmentarze.',
+' 11.11.25 w Dzień Niepodległości Msza o 10.00.',
+' W następną niedzielę wspominamy Bł. Męczenniczki s. Katarzynki i po Mszach adoracja relikwi. ', 
+' W przyszłą niedzielę ofiary na fundusz remontowy.',
+
 
 ];
