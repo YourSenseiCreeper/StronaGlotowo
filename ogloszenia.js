a201edecb0dbcb24197eb96461b1bf4929f0d3eb
mproszek <mproszek@op.pl>
2025-11-15 20:21:29 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,9 @@
-var ogloszeniaNaglowek = '32 NIEDZIELA ZWYKŁA C - 9.11.2025 R.';
+var ogloszeniaNaglowek = '33 NIEDZIELA ZWYKŁA C - 16.11.2025 R.';
 var ogloszenia = [
  
-' 11.11.25 w Dzień Niepodległości Msza o 10.00.',
-' W następną niedzielę wspominamy Bł. Męczenniczki s. Katarzynki i po Mszach adoracja relikwi. ', 
-' W przyszłą niedzielę ofiary na fundusz remontowy.',
+' Bóg zapłać dziś za przygotowanie uroczystości, szczególnie Siostrom. Zapraszamy na obiad.',
+' Dziś w niedzielę wspominamy Bł. Męczenniczki s. Katarzynki i po Mszach adoracja relikwi. ', 
+' Dziś w niedzielę ofiary na fundusz remontowy.',
 
 
 ];
