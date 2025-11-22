2d1e9773c33c99c8478a5f3fa2624e783bb89335
mproszek <mproszek@op.pl>
2025-11-22 20:22:16 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,8 @@
-var ogloszeniaNaglowek = '33 NIEDZIELA ZWYKŁA C - 16.11.2025 R.';
+var ogloszeniaNaglowek = '34 NIEDZIELA CHRYSTUSA KRÓLA C - 23.11.2025 R.';
 var ogloszenia = [
  
-' Bóg zapłać dziś za przygotowanie uroczystości, szczególnie Siostrom. Zapraszamy na obiad.',
-' Dziś w niedzielę wspominamy Bł. Męczenniczki s. Katarzynki i po Mszach adoracja relikwi. ', 
-' Dziś w niedzielę ofiary na fundusz remontowy.',
+' Dziś po Sumie nabożeństwo Eucharystyczne zawierzenia Chrstusowi Królowi.',
+' W przyszłą niedzielę rozpoczynamy Adwent. ',
 
 
 ];
