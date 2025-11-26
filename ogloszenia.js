fb4b656a4b7d4a34242352e8c66487a7c90f7e60
mproszek <mproszek@op.pl>
2025-11-26 19:57:10 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,10 @@
-var ogloszeniaNaglowek = '34 NIEDZIELA CHRYSTUSA KRÓLA C - 23.11.2025 R.';
+var ogloszeniaNaglowek = '1 NIEDZIELA ADWENT A - 30.11.2025 R.';
 var ogloszenia = [
  
-' Dziś po Sumie nabożeństwo Eucharystyczne zawierzenia Chrstusowi Królowi.',
-' W przyszłą niedzielę rozpoczynamy Adwent. ',
+' Dziś rozpoczynamy Adwent czas radosnego oczekiwania na Przyjście Pana.',
+' W tym tygodniu wypada 1 piątek i 1 sobota miesiąca, zapraszam w tych dniach w piątek i w sobotę na Msze Roratnie z nabożeństwami dzieci, młodzież i dorosłych z lapionami. ',
+' W przyszłą niedzielę Msza w intencji Róż Różańcowych i parafian o 8.30, po niej Adoracja Eucharystyczna i zmiana tajemnic różańca.',
+' W zakrystii są do nabycia świece Caritas na stół wigilijny jako pomoc dla ubogich dzieci.',
 
 
 ];
