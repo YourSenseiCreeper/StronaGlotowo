4d94f451234ff7716ca9fe2398cab101cb37fc68
mproszek <mproszek@op.pl>
2025-12-06 23:27:53 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,10 +1,9 @@
-var ogloszeniaNaglowek = '1 NIEDZIELA ADWENT A - 30.11.2025 R.';
+var ogloszeniaNaglowek = '2 NIEDZIELA ADWENT A - 7.12.2025 R.';
 var ogloszenia = [
  
-' Dziś rozpoczynamy Adwent czas radosnego oczekiwania na Przyjście Pana.',
-' W tym tygodniu wypada 1 piątek i 1 sobota miesiąca, zapraszam w tych dniach w piątek i w sobotę na Msze Roratnie z nabożeństwami dzieci, młodzież i dorosłych z lapionami. ',
-' W przyszłą niedzielę Msza w intencji Róż Różańcowych i parafian o 8.30, po niej Adoracja Eucharystyczna i zmiana tajemnic różańca.',
-' W zakrystii są do nabycia świece Caritas na stół wigilijny jako pomoc dla ubogich dzieci.',
+' W tym tygodniu zapraszam dzieci, młodzież i dorosłych z lapionami na Msze Roratnie. ',
+' Dziś w niedzielę Msza w intencji Róż Różańcowych i parafian o 8.30, po niej Adoracja Eucharystyczna i zmiana tajemnic różańca.',
+' W zakrystii są do nabycia świece Caritas na stół wigilijny jako pomoc dla ubogich dzieci. W zakrystii i w klasztorze Sióstr Katarzynek można nabyć poświęcone opłatki wigilijny.',
 
 
 ];
