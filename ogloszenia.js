31b94d9e97b1c37671b9b0754964b607815ba560
mproszek <mproszek@op.pl>
2025-12-20 20:43:42 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,9 @@
-var ogloszeniaNaglowek = '3 NIEDZIELA ADWENT A - 14.12.2025 R.';
+var ogloszeniaNaglowek = '4 NIEDZIELA ADWENT A - 21.12.2025 R.';
 var ogloszenia = [
  
-' W tym tygodniu zapraszam dzieci, młodzież i dorosłych z lampionami na Msze Roratnie we wtorek i czwartek o 18.00. ',
-' W sobotę 20.12.25 będzie Adwentowy dzień Rekolekcyjny - Msze z nauką i okazją do spowiedzi o 11.00 i 18.00. Od 13.00 planuję odwiedzić chorych. Proszę o ich zgłaszanie. Rekolekcje poprowadzi ks. Jan Sztygiel Proboszcz z Rogóża. Ofiary przeznaczamy na cele jego parafii. Można przekazać dary na stół rekolekcyjny.',
-' Dziś w niedzielę CAW i Sołectwo zaprasza na wigilię wiejską o 16.00 w sali CAW.',
+' W tym tygodniu zapraszam dzieci, młodzież i dorosłych z lampionami na Msze Roratnie we wtorek o 18.00. ',
+' W tym tygodniu będziemy przeżywać Wigilię i święta Narodzenia Pańskiego.W czasi wigilii nie zapomnijmy o czytaniu Ewangelii o Narodzeniu, modlitwy i śpiewu kolęd. Pasterka o północy. Suma w Boże Narodzenie o 11.00. W II dzień świąt Msze o 8.30 i 11.00. Ofiary na KUL.',
+' Bóg zapłać za dary na stół rekolekcyjny i pomoc w przygotowaniu tego dnia.',
 ' W zakrystii są do nabycia świece Caritas na stół wigilijny jako pomoc dla ubogich dzieci - koszt 10 zł. W zakrystii i w klasztorze Sióstr Katarzynek można nabyć poświęcone opłatki wigilijny.',
 
 
