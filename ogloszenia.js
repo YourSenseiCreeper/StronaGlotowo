b585167feffc6f6de6878c688c6c9be89d00e180
mproszek <mproszek@op.pl>
2026-01-03 21:31:54 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,11 +1,9 @@
-var ogloszeniaNaglowek = 'NIEDZIELA ŚWIĘTEJ RODZINY A - 21.12.2025 R.';
+var ogloszeniaNaglowek = '2 NIEDZIELA BOŻEGO NARODZENIA A - 4.01.2026 R.';
 var ogloszenia = [
  
-' W tym tygodniu w sylwestra Msza na zakończenie roku z nabożeństwem dziękczynno-błagalnym o 10.00. ',
-' W tym tygodniu 1.01. Uroczystość Świętej Bożej Rodzicielki Msze o 11.00 i 18.00.',
-' Wypada też I piątek miesiąca Msza z nabożeństwem o 18.00 a w I sobotę o 9.00.',
-' W sobotę zaczynamy kolędę - wizytę duszpasterską od Swobodna od g. 10.00.',
-' W następną niedzielę Msza w intencji Róż Różańcowych i Parafian o 8.30, po niej Adoracja Eucharystyczna i zmiana tajemnic. Po sumie zapraszam na spotkanie opłatkowo-kolędowe na które zapraszam członków Róż Różańcowych i Rad Parafialnych oraz wszystkich chętnych.',
+' W tym tygodniu 6.01. Uroczystość Objawienia Pańskiego - Trzech Króli Msze z błogosławieństwem kredy o 8.30 i 11.00.',
+' Kontynujemy kolędę - wizytę duszpasterską; w poniedziałek kol.pn od p.Napieralskich do p.Milkiewicz, od g. 10.00, trans. Ks. Adam Napieralski, w środe kol. pn od p.Dawidowicz do p. Pusz, od 10.00, trans. p. Daniel Symonowicz, w czwartek kol. pd od p. Dąbrowskich do p. Stefańskich od 10.00, trans. p. Andrzej Stefański, piątek kol. pn od p. Stanulewicz do p. Bibik, od 10.00, trans. p. Adam Brzezińscy, w sobotę ul Kościeln od p.Kirkis do p.Wołodżko od 10.00.',
+' Dziś w niedzielę Msza w intencji Róż Różańcowych i Parafian o 8.30, po niej Adoracja Eucharystyczna i zmiana tajemnic. Po sumie zapraszam na spotkanie opłatkowo-kolędowe na które zapraszam członków Róż Różańcowych i Rad Parafialnych oraz wszystkich chętnych.',
 
 
 ];
