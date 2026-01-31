2f7388829d0ad3b77a8883f092fce1b87ed8f6ed
mproszek <mproszek@op.pl>
2026-01-31 16:46:47 +0100

    Test

--- aktualnosci.js
+++ aktualnosci.js
@@ -1,37 +1,37 @@
-var aktualnosciNaglowek = 'AKTUALNOŚCI 2025 R.';
+var aktualnosciNaglowek = 'AKTUALNOŚCI 2026 R.';
 var aktualnosci = [
-    new Aktualnosc('PIĄTKI WIELKIEGO POSTU 2025', 'GODZINY MIŁOSIERDZIA BOŻEGO',
+    new Aktualnosc('PIĄTKI WIELKIEGO POSTU 2026', 'GODZINY MIŁOSIERDZIA BOŻEGO',
     [
         '17.00 - DROGA KRZYŻOWA NA KALWARII WARMIŃSKIEJ,',
         '18.00 - MSZA ŚW Z MODLITWĄ O UZDROWIENIE DUSZY I CIAŁA.'
     ]),
-    new Aktualnosc('10-12.04.2025', 'REKOLEKCJE WIELKOPOSTNE',
+    new Aktualnosc('27-28.03.2026', 'REKOLEKCJE WIELKOPOSTNE',
     [
         '- 11.00 - Msza Św. z nauką rekolekcyjną.',
         '- 18.00 - Msza Św. z nauką rekolekcyjną.'
     ]),
   
-    new Aktualnosc('17.05.2025', '131 ROCZNICA POŚWIĘCENIA KALWARII WARMIŃSKIEJ I PIELGRZYMKA SOLIDARNOŚCI W HOŁDZIE śW. JANOWI PAWŁOWI II.',
+    new Aktualnosc('16.05.2026', '132 ROCZNICA POŚWIĘCENIA KALWARII WARMIŃSKIEJ I PIELGRZYMKA SOLIDARNOŚCI W HOŁDZIE śW. JANOWI PAWŁOWI II.',
     [
         '- 11.00 - Uroczysta Msza Święta',
         '- 12.00 - Droga Krzyżowa'
     ]),
-    new Aktualnosc('22.06.2025', 'Odpust diecezjalny w Głotowie.',
+    new Aktualnosc('7.06.2026', 'Odpust diecezjalny w Głotowie.',
     [
 		'- 8.30 - Msza Św. z kazaniem',
         '- 10.00 - Droga Krzyżowa',
         '- 11.00 - Adoracja Najświętszego Sakramentu',
         '- 12.00 - Suma odpustowa'
     ]),
-    new Aktualnosc('21.09.2025', 'Odpust diecezjalny w Głotowie',
+    new Aktualnosc('20.09.2026', 'Odpust diecezjalny w Głotowie',
     [
         '- 8.30 - Msza Św. z kazaniem',
         '- 10.00 - Droga Krzyżowa',
         '- 11.00 - Adoracja Najświętszego Sakramentu',
         '- 12.00 - Suma odpustowa'
     ]),
-    new Aktualnosc('28.09.2025', 'Dożynki parafialne - Msza Św. o 11.00.', []),
-    new Aktualnosc('20.12.2025', 'ADWENTOWY DZIEŃ REKOLEKCYJNY.',
+    new Aktualnosc('27.09.2026', 'Dożynki parafialne - Msza Św. o 11.00.', []),
+    new Aktualnosc('19.12.2026', 'ADWENTOWY DZIEŃ REKOLEKCYJNY.',
     [
         '- Okazja do spowiedzi w czasie Mszy Świętych.',
         '- 11.00 - Msza Św. i nauka rekolekcyjna.',
