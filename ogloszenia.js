df59ee7408ad2635947ae611a1b32c730824cdb2
mproszek <mproszek@op.pl>
2026-02-14 23:05:53 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,7 +1,8 @@
-var ogloszeniaNaglowek = '4 NIEDZIELA ZWYKŁ A - 1.02.2026 R.';
+var ogloszeniaNaglowek = '6 NIEDZIELA ZWYKŁ A - 15.02.2026 R.';
 var ogloszenia = [
  
-' Jutro Ofiarowanie Pański-Matki Bozej Gromnicznej - Msza z błogosławieństwiem gromnic i procesją o 11.00 i 18.00. Ofiary na Karmelitanki w Spręcowie.',
-' W tym tygodniu wypada I piątek miesiąca.Msza z nabożeństwem o 8.00. Dziś po Mszy o 8.30 Adpracja Eucharystyczna i zmiana tajemnic',
+' W tym tygodniu w Środę Popielcową Msze z posypaniem głó popiołem o 11.00 i 18.00. Dziś ofiary na Ukrainę.',
+' W tym tygodniu w piątek rozpoczynamy nasze Godziny Miłosierdzia Bożego Drogą Krzyżową na Kalwarii o 17.00, po niej Msza z modlitwą o uzdrowienie duszy i ciała. Uprzejmie proszę o przygotowanie kaplic do nabożeństw.',
+' W przyszłą niedzielę po Sumie Gorzkie Żale, a po nich spotkanie z rodzicami i dziećmi prze I Komunią .',
 
 ];
