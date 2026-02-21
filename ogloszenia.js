3288efefd551aec6fa554d991e730adc3d884dda
mproszek <mproszek@op.pl>
2026-02-22 00:02:22 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,8 @@
-var ogloszeniaNaglowek = '6 NIEDZIELA ZWYKŁ A - 15.02.2026 R.';
+var ogloszeniaNaglowek = '1 NIEDZIELA WIELKOPOSTNA A - 22.02.2026 R.';
 var ogloszenia = [
  
-' W tym tygodniu w Środę Popielcową Msze z posypaniem głó popiołem o 11.00 i 18.00. Dziś ofiary na Ukrainę.',
-' W tym tygodniu w piątek rozpoczynamy nasze Godziny Miłosierdzia Bożego Drogą Krzyżową na Kalwarii o 17.00, po niej Msza z modlitwą o uzdrowienie duszy i ciała. Uprzejmie proszę o przygotowanie kaplic do nabożeństw.',
-' W przyszłą niedzielę po Sumie Gorzkie Żale, a po nich spotkanie z rodzicami i dziećmi prze I Komunią .',
+' W przyszłą niedzielę Msza w intencji Róż Różańcowych i Parafian o 8.30, po niej zmiana tajemnic różańca i Gorzkie Żale.',
+' W tym tygodniu w piątek przeżywamy Godziny Miłosierdzia Bożego, początek Drogą Krzyżową na Kalwarii o 17.00, po niej Msza z modlitwą o uzdrowienie duszy i ciała. Dziękuję za przygotowanie kaplic do nabożeństw.',
+' Dziś w niedzielę po Sumie Gorzkie Żale, a po nich spotkanie z rodzicami i dziećmi prze I Komunią. Sprzątanie.',
 
 ];
