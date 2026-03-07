1f71afaac59ede7bef453bd01a1f3ac55c822020
mproszek <mproszek@op.pl>
2026-03-07 22:57:30 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -2,7 +2,7 @@ var ogloszeniaNaglowek = '3 NIEDZIELA WIELKOPOSTNA A - 8.03.2026 R.';
 var ogloszenia = [
  
 ' Dziś po Sumie Gorzkie Żale.',
-' W tym tygodniu w piątek przeżywamy Godziny Miłosierdzia Bożego, początek Drogą Krzyżową na Kalwarii o 17.00, po niej Msza z modlitwą o uzdrowienie duszy i ciała. Dziękuję za przygotowanie kaplic do nabożeństw.',
+' W piątek zapraszamy na Godziny Miłosierdzia Bożego, początek Drogą Krzyżową na Kalwarii o 17.00, po niej Msza z modlitwą o uzdrowienie duszy i ciała. Dziękuję za przygotowanie kaplic do nabożeństw.',
 ' Ofiary na kwiaty do grobu pańskiego można składać na ołtarzu Matki Bożej.',
 ' Bóg zapłać wszystkim ofiarodawcom, którzy zakupili pulpit pod mszał. Sprzątanie.',
 
