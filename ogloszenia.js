51ac6d7317520e6cdea68f5bb99f1e133dedab59
mproszek <mproszek@op.pl>
2026-03-07 22:48:15 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,9 @@
-var ogloszeniaNaglowek = '1 NIEDZIELA WIELKOPOSTNA A - 22.02.2026 R.';
+var ogloszeniaNaglowek = '3 NIEDZIELA WIELKOPOSTNA A - 8.03.2026 R.';
 var ogloszenia = [
  
-' W przyszłą niedzielę Msza w intencji Róż Różańcowych i Parafian o 8.30, po niej zmiana tajemnic różańca i Gorzkie Żale.',
+' Dziś po Sumie Gorzkie Żale.',
 ' W tym tygodniu w piątek przeżywamy Godziny Miłosierdzia Bożego, początek Drogą Krzyżową na Kalwarii o 17.00, po niej Msza z modlitwą o uzdrowienie duszy i ciała. Dziękuję za przygotowanie kaplic do nabożeństw.',
-' Dziś w niedzielę po Sumie Gorzkie Żale, a po nich spotkanie z rodzicami i dziećmi prze I Komunią. Sprzątanie.',
+' Ofiary na kwiaty do grobu pańskiego można składać na ołtarzu Matki Bożej.',
+' Bóg zapłać wszystkim ofiarodawcom, którzy zakupili pulpit pod mszał. Sprzątanie.',
 
 ];
