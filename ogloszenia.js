3c07cc60163cdf3840d0be4f248a906590bf1736
mproszek <mproszek@op.pl>
2026-03-22 00:35:18 +0100

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,8 +1,9 @@
-var ogloszeniaNaglowek = '4 NIEDZIELA WIELKOPOSTNA A - 15.03.2026 R.';
+var ogloszeniaNaglowek = '5 NIEDZIELA WIELKOPOSTNA A - 22.03.2026 R.';
 var ogloszenia = [
  
 ' Dziś po Sumie Gorzkie Żale.',
-' W piątek zapraszamy na Godziny Miłosierdzia Bożego, początek Drogą Krzyżową na Kalwarii o 17.00, po niej Msza z modlitwą o uzdrowienie duszy i ciała. Dziękuję za przygotowanie kaplic do nabożeństw.',
+' W piątek zapraszamy na Godziny Miłosierdzia Bożego, początek Drogą Krzyżową na Kalwarii o 17.00, po niej Msza z modlitwą o uzdrowienie duszy i ciała. ',
 ' Ofiary na kwiaty do grobu pańskiego można składać na ołtarzu Matki Bożej.',
-
+' W tym tygodniu będziemy przeżywać nasze rekolekcje wielkopostne. Msze będą w piątek i sobotę o 11.00 z nauką dla dzieci i o 18.00. W czasie Mszy okazja do spowiedzi. Odwiedzę chorych w piątek po 13.00. Można ich zgłaszać. Uprzejmie proszę o dary na stół rekolekcyjny a w sobotę ofiary na dzieła które prowadzi ks. rekolekcjonista Janusz Gajdowski z Olsztyna.',
+' W przyszłą niedzielę Uroczystość Męki Pańskiej-Niedziela Palmowa-Msze z poświęceniem Palm o 8.30 i 11.00',
 ];
