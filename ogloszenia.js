a47ecf77a20a31de89ab36ed781f511097bc70c4
mproszek <mproszek@op.pl>
2026-04-04 15:56:29 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,12 +1,9 @@
-var ogloszeniaNaglowek = 'NIEDZIELA PALMOWA A - 29.03.2026 R.';
+var ogloszeniaNaglowek = 'NIEDZIELA ZMARTWYCHWSTANIA A - 5.04.2026 R.';
 var ogloszenia = [
  
-' Bóg zapłać za przygotowanie rekolekcji w kuchni i za dary na stół odpustowy. Bóg zapłać naszej szkole za pomoc i udział.',
-' Dziś Uroczystość Męki Pańskiej - N. Palmowa. Msze z poświęceniem palm i procesją o 8.30 i 11.00, po Sumie Gorzkie Żale. ',
-' Ofiary na kwiaty do grobu Pańskiego można składać na ołtarzu Matki Bożej.',
-' W tym tygodniu będziemy uczestniczyć w Triduum Paschalnym. W wielki Czwartek Msza Wieczerzy Pańskiej o 18.00, po niej początek adoracji P. Jezusa w ciemnicy. W wielki Piątek adoracja P. Jezusa w Ciemnicy od 10.00. Zapraszam poszczególne osoby od 10-11 z ul. Szkolnej, od 11-12 z ul Kościelnej, 12-13 z kol. pd, 13-14 z kol pn, 14-15 Swobodna, o 17.00 Droga Krzyżowa na Kalwarii, po niej Liturgia Męki Pańskiej i Adoracja P. Jezusa w Grobie do 21.',
-' W Wielką Sobotę adoracja P. Jezusa w Grobie od 10. Zapraszam poszczególne osoby od 10-11 z ul. Szkolnej, od 11-12 z ul Kościelnej, 12-13 z kol. pd, 13-14 z kol pn, 14-15 Swobodna, o 18.00 Liturgia Wigilii Paschalnej z odnowieniem ze świecami przyrzeczeń chrzcielnych, po niej adoracja P. Jezusa w grobie do 21. Poświęcenie pokarmów wielkanocnych w Swobodnie o 11, w Głotowie o 12 i po Wigilii Paschalnej',
-' W Niedzielę Zmartwychwstania Rezurekcje z procesją o 6.00 i Suma Wielkanocna o 11.00. ',
-' Zapraszam do przygotowania Grobu i Ciemnicy strażaków i chętnych we wtorek o 15.00. Zapraszam dzieci, młodzież i dorosłych a szczególnie Strażaków do przygotowania adoracji i procesji.',
+' Życzenia. Bóg zapłać za przygotowanie uroczystości.',
+' Dziś jeszcze adoracja P. Jezusa w grobie do 21. ,
+' Jutro Rezurekcje z procesją o 6.00 i Suma Wielkanocna o 11.00. ',
+' W Poniedziałek Wiekanocny II dzień świąt Msza o 8.30 w intencji Róż Różańcowych i Parafian o 8.30, po niej adoracja Eucharystyczna i zmiana tajemnic. O 11.00 druga Msza. Ofiary zbieramy na KUL.',
 
 ];
