cde425a98a12603aacbfd3822f0055f875d24502
mproszek <mproszek@op.pl>
2026-04-11 23:05:42 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,9 +1,7 @@
-var ogloszeniaNaglowek = 'NIEDZIELA ZMARTWYCHWSTANIA A - 5.04.2026 R.';
+var ogloszeniaNaglowek = 'NIEDZIELA MIŁOSIERDZIA A - 12.04.2026 R.';
 var ogloszenia = [
  
-' Życzenia. Bóg zapłać za przygotowanie uroczystości.',
-' Dziś jeszcze adoracja P. Jezusa w grobie do 21. ,
-' Jutro Rezurekcje z procesją o 6.00 i Suma Wielkanocna o 11.00. ',
-' W Poniedziałek Wiekanocny II dzień świąt Msza o 8.30 w intencji Róż Różańcowych i Parafian o 8.30, po niej adoracja Eucharystyczna i zmiana tajemnic. O 11.00 druga Msza. Ofiary zbieramy na KUL.',
+' BÓG ZAPŁAĆ ZA OFIARY NA CARITAS. ZA TYDZIEŃ NA FUNDUSZ REMONTOWY',
+' CHETNI, KTÓRZY CHCIELIBY ŚPIEWAĆ W SCHOLI PARAFIALNEJ SĄ ZAPROSZENI DO DOMU ZAKONNEGO W CZWARTEK O 18.30. ',
 
 ];
