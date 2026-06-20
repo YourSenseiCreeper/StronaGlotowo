c079014a8aab31a2d6e47141a8302a9ec4b06a54
mproszek <mproszek@op.pl>
2026-06-20 23:32:45 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,6 +1,7 @@
-var ogloszeniaNaglowek = '11 NIEDZIELA ZWYKŁA A - 14.06.2026 R.';
+var ogloszeniaNaglowek = '12 NIEDZIELA ZWYKŁA A. - 21.06.2026 R.';
 var ogloszenia = [
-' ZAPRASZAMY NA NABOŻEŃSTWA CZERWCOWE PO MSZY O 18.00 ',
-' DIĘKUJĘ ZA PRZYGOTOWANIE UROCZYSTOŚCI ODPUSTOWE. ',
+' ZAPRASZAMY NA NABOŻEŃSTWA CZERWCOWE PO MSZY O 18.00, A DZIŚ PO SUMIE. ',
+' DIĘKUJĘ ZA PRZYGOTOWANIE KWIATÓW. ',
+' MSZA NA ZAKOŃCZENIE ROKU SZKOLNEGO WE CZWARTEK O 9.00. PRZED MSZĄ OKAZJA DO SPOWIEDZI.',
 ' W IMIENIU P. GRZEGORZA Z OSTRÓDY ZAPRASZAM NA PIELGRZYMKI 1. Do Medjugorie w terminie 27.06 – 04.07.2026, Koszt wyjazdu: 1100 zł i 280 euro,do Włoch – rejon Apulii w terminie 09-19.07.2026,Koszt: 1400 zł i 800 euro i do Hiszpannii i Portugalii (Fatima i Santiago de Compostella) w dniach 22-29.09.26 w cenie 2400 zł i 790 euro. Informacje na tablicy odłoszeń.',
 ];
