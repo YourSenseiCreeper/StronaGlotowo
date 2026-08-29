351cfb1646fb7b9a352b2c06ce21317a556524ed
mproszek <mproszek@op.pl>
2026-08-29 23:07:31 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,7 +1,6 @@
-var ogloszeniaNaglowek = '19 NIEDZIELA ZWYKŁA A. - 9.08.2026 R.';
+var ogloszeniaNaglowek = '22 NIEDZIELA ZWYKŁA A. - 30.08.2026 R.';
 var ogloszenia = [
-' DIĘKUJĘ ZA PRZYGOTOWANIE KWIATÓW W KOŚCIELE POSPRZĄTANIE KOŚCIOŁA. ',
-' 15.08. WYPADA UROCZYSTOŚĆ WNIEBOWZIĘCIA MATKI BOŻEJ -MSZE Z POŚWIĘCENIEM KWIATÓW I ZIÓŁ O 8.30 I 11.00.',
-' ZAKOŃCZYLIŚMY KONSERWACJĘ OSTATNIEJ CZĘŚCI POLICHROMII ŚCIANY PD, KOSZT NASZEGO WKŁADU TO 60 TYS. BÓG ZAPŁAĆ ZA OFIARY SKŁADANE NA TEN CEL.',
+' 1.09. we wtorek Msza na rozpoczęcie roku szkolnego o 9.00 z poświęceniem tornistrów dla pierwszoklasistów.',
+' W tym tygodniu wypada I piątek i I sobota miesiąca. Msze z nabożeństwami o 18.00. W następną niedzielę Msza w intencji Róż Różańcowych i Parafian o 8.30, po niej Adoracja Eucharystyczna i zmiana tajemnic.',
 ' W IMIENIU P. GRZEGORZA Z OSTRÓDY ZAPRASZAM NA PIELGRZYMKĘ Do Portugalii i Hiszpanii (Fatima i Santiago de Compostella) w dniach 22-29.09.26 w cenie 2400 zł i 790 euro. Informacje na tablicy odłoszeń.',
 ];
