a98fced253b2d17f3d8c954b99f51c2e4c354ece
mproszek <mproszek@op.pl>
2026-09-19 23:37:56 +0200

    Test

--- ogloszenia.js
+++ ogloszenia.js
@@ -1,6 +1,6 @@
-var ogloszeniaNaglowek = '24 NIEDZIELA ZWYKŁA A. - 13.09.2026 R.';
+var ogloszeniaNaglowek = ' NIEDZIELA ODPUSTPWA A. - 20.09.2026 R.';
 var ogloszenia = [
-' W następną niedzielę będziemy przeżywać Uroczystości odpustowe. o8.30 Msza z kazaniem, o 10.00 Droga Krzyżowa na Kalwarii, o 11.00 Adoracja z róąńcem, o 12.00 Procesja i Suma Odpustowa. Uroczystości poprowadzi ks. Andrzej Migrała , Misjonarz z Boliwii. Ofiary przeznaczamy na Misje w parafii ks. Andrzeja w Boliwii..',
-' W związku z uroczystością proszę o przygotowanie terenu kościoła i kaplic Kalwarii, procesji - o udział OSP i o dary na stół odpustowy np. ciasta .Nasze dorzynki zaplanowane zostały na sobotę 26.09. o 16.00',
-' W IMIENIU P. GRZEGORZA Z OSTRÓDY ZAPRASZAM NA PIELGRZYMKĘ Do Portugalii i Hiszpanii (Fatima i Santiago de Compostella) w dniach 22-29.09.26 w cenie 2400 zł i 790 euro. Informacje na tablicy odłoszeń.',
+' Dziś niedzielę będziemy przeżywać Uroczystości odpustowe. o8.30 Msza z kazaniem, o 10.00 Droga Krzyżowa na Kalwarii, o 11.00 Adoracja z róąńcem, o 12.00 Procesja i Suma Odpustowa. Uroczystości poprowadzi ks. Andrzej Migrała , Misjonarz z Boliwii. Ofiary przeznaczamy na Misje w parafii ks. Andrzeja w Boliwii..',
+' W związku z uroczystością dziękuję za przygotowanie terenu kościoła i kaplic Kalwarii, procesji - udział OSP za dary na stół odpustowy np. ciasta .Nasze dorzynki wraz z dziękczynieniem za 80 lat OSP Dłotowo zaplanowane zostały na sobotę 26.09. o 16.00',
+' Dziś przy kaplicy XIV można zamówić intencje mszalne, a herbaciarni zapraszamy na zupę, ciostko, kawę i herbatę tam też można nabyć pamiątki z Glotowa i pakiety Gietrzwałdzkie.',
 ];
